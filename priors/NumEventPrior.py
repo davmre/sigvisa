1afbd6ebccc173dae8ef0947d74935107c9df4b7
nimar.arora <nimar.arora@334f63d7-9462-3ba8-66c2-e6a200314d38>
2010-03-22 19:43:05 +0000

    added EventLocationPrior and moved config to parameters

--- priors/NumEventPrior.py
+++ priors/NumEventPrior.py
@@ -1,10 +1,10 @@
 import os
 
-def learn(config_dirname, start_time, end_time, leb_events):
+def learn(param_fname, start_time, end_time, leb_events):
   rate = float(len(leb_events)) / (end_time - start_time)
   print ("event rate is %f per second or %.1f per hour"
          % (rate, rate * 60 * 60))
-  fp = open(os.path.join(config_dirname,"NumEventPrior.txt"), "w")
+  fp = open(os.path.join(param_fname,), "w")
   
   print >>fp, rate
   
