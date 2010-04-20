7f4302828adb93ee03f1b23ea2a76cf5156d66ee
nimar.arora <nimar.arora@334f63d7-9462-3ba8-66c2-e6a200314d38>
2010-04-20 22:19:01 +0000

    sel3roc can draw multiple curves on one figure

--- sel3roc.py
+++ sel3roc.py
@@ -17,9 +17,9 @@ def draw_roc(label, fname):
     
     istrue = bool(int(istrue))
     score = float(score)
-
+    
     scores.append((score, istrue))
-
+    
     if istrue:
       total_true += 1
     else:
@@ -27,7 +27,7 @@ def draw_roc(label, fname):
   fp.close()
   
   scores.sort(reverse=True)
-
+  
   # compute the ROC curve
   x_pts, y_pts = [], []
   num_true, num_false = 0, 0
@@ -37,7 +37,7 @@ def draw_roc(label, fname):
       num_true += 1
     else:
       num_false += 1
-
+    
     if cnt % 30 == 0 or cnt == len(scores)-1:
       y_pts.append(float(num_true) / total_true)
       x_pts.append(float(num_false) / total_false)
@@ -45,17 +45,19 @@ def draw_roc(label, fname):
   plt.plot(x_pts, y_pts, label=label)
 
 def main():
-  if len(sys.argv) != 3:
-    print "Usage: sel3roc.py <label> <score-file-name>"
+  if (len(sys.argv)-1) % 2 != 0 or sys.argv < 3:
+    print "Usage: sel3roc.py <label> <score-file-name> "\
+          "[<label> <score-file>].."
     sys.exit(1)
   
   plt.figure()
   plt.title("ROC curve for SEL3 events")
+
+  for i in range(1, len(sys.argv), 2):
+    label = sys.argv[i]
+    fname = sys.argv[i+1]
   
-  label = sys.argv[1]
-  fname = sys.argv[2]
-  
-  draw_roc(label, fname)
+    draw_roc(label, fname)
 
   plt.xlim(0, 1)
   plt.ylim(0, 1)
