11df60a520cf1b9efa610e599166848eb4e7cb47
nimar.arora <nimar.arora@334f63d7-9462-3ba8-66c2-e6a200314d38>
2011-04-15 15:19:55 +0000

    switch from count based invert to time based when more than 1500 detections
    git-svn-id: http://netvisa.googlecode.com/svn/trunk@151 334f63d7-9462-3ba8-66c2-e6a200314d38

--- infer/propose.h
+++ infer/propose.h
@@ -5,6 +5,10 @@ int propose_hough(NetModel_t * p_netmodel, Event_t **pp_events,
                   int det_high, double degree_step, double time_step);
 
 
-int propose_invert(NetModel_t * p_netmodel, Event_t **pp_events,
-                   double time_low, double time_high, int det_low,
-                   int det_high, double degree_delta, int num_secs);
+int propose_invert_timed(NetModel_t * p_netmodel, Event_t **pp_events,
+                         double time_low, double time_high, int det_low,
+                         int det_high, double degree_delta, int num_secs);
+
+int propose_invert_step(NetModel_t * p_netmodel, Event_t **pp_events,
+                        double time_low, double time_high, int det_low,
+                        int det_high, double degree_step, int num_step);
