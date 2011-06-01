173b823ec84252e5589c1d322224c29386c973b8
nimar.arora <nimar.arora@334f63d7-9462-3ba8-66c2-e6a200314d38>
2011-06-01 17:48:27 +0000

    load script for tohoku tables

---
+++ database/tohoku.sql
@@ -0,0 +1,15 @@
+/*
+ * Load the tohoku specific tables into the ctbt3mos database for testing
+ */
+load data local infile 'tohoku_idcx_arrival.csv' into table idcx_arrival fields terminated by ',' lines terminated by '\r\n' ignore 1 lines;
+
+load data local infile 'tohoku_leb_origin.csv' into table leb_origin fields terminated by ',' lines terminated by '\r\n' ignore 1 lines;
+
+load data local infile 'tohoku_leb_assoc.csv' into table leb_assoc fields terminated by ',' lines terminated by '\r\n' ignore 1 lines;
+
+load data local infile 'tohoku_sel3_origin.csv' into table sel3_origin fields terminated by ',' lines terminated by '\r\n' ignore 1 lines;
+
+load data local infile 'tohoku_sel3_assoc.csv' into table sel3_assoc fields terminated by ',' lines terminated by '\r\n' ignore 1 lines;
+
+insert into dataset values ('tohoku', 1299801600, 1299974400);
+commit;
