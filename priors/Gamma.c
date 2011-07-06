a4b236fb3d1fdec7f0be3787774f54be56694f77
nimar.arora <nimar.arora@334f63d7-9462-3ba8-66c2-e6a200314d38>
2011-07-06 06:28:08 +0000

    missing files for KS test

---
+++ priors/Gamma.c
@@ -0,0 +1,36 @@
+#include <math.h>
+
+#include "../netvisa.h"
+
+static double loggamma(double num)
+{
+  return (num - .5) * log(num) - num + .5 * log(2 * M_PI);  
+}
+
+double Gamma_logprob(double val, double shape, double scale)
+{
+  double logprob= (shape-1) * log(val) - val / scale - loggamma(shape)
+    - shape * log(scale);
+
+  if (isinf(logprob))
+  {
+    printf("logprob isinf: val %lg, shape %lg, scale %lg\n",
+           val, shape, scale);
+    exit(1);
+  }
+  
+  return logprob;
+  
+}
+
+double Gamma_cdf(double val, double shape, double scale)
+{
+  double sum=0;
+  double delta = val / 100.0;
+  double i;
+  
+  for (i=delta; i<val; i+=delta)
+    sum += exp(Gamma_logprob(i, shape, scale));
+  
+  return sum * delta;
+}
---
+++ utils/kstest.py
@@ -0,0 +1,25 @@
+# Kolmogorov-Smirnov goodness of fit test
+
+import numpy as np
+
+def kstest(values, cdf):
+  values = [v for v in values]          # copy the values
+  values.sort()                         # .. before sorting them
+  
+  #import pdb
+  #pdb.set_trace()
+
+  stat = -np.inf
+  for c,v in enumerate(values):
+    tp = cdf(v)
+    ep = (c+1.) / len(values)
+    
+    diff = abs(tp - ep)
+    
+    if diff > stat:
+      stat = diff
+  
+  stat *= np.sqrt(len(values))
+  
+  return stat <= 1.358, stat
+
