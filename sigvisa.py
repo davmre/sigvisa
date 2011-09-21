b00d6a756516c703a9157b0308c94db4f11c4d02
David Moore <davmre@gmail.com>
2011-09-21 03:45:53 -0700

    checkpointing work on integrating over travel times. It doesn't currently work, but I did write lots of code.

--- sigvisa.py
+++ sigvisa.py
@@ -45,7 +45,7 @@ class SigModel:
             newmean = newmean - 1/(2*hz)
         return np.array(envelopem), np.array(envelopev)
 
-    def all_envelopes(self, stats, events):
+    def all_envelopes(self, stats, events, ttimes):
         """
         Combine information from all events at a single station, and return the resulting mean and variance.
         """
@@ -57,12 +57,11 @@ class SigModel:
         means = np.ones((npts, 1))*self.noise_mean
         variances = np.ones((npts, 1))*self.noise_variance
 
-        for event in events:
+        for (event, ttime) in zip(events, ttimes):
             if len(event) < 4:
                 continue
 
             rel_time = event[3] - stats['starttime'].getTimeStamp()
-            ttime = self.netmodel.mean_travel_time(event[0], event[1], event[2], siteid, 0)
             (emeans, evars) = self.envelope(event, siteid, samprate)
 
             print "event at t=", rel_time+ttime, ", mb = ", event[4]
@@ -70,10 +69,11 @@ class SigModel:
             for i in range(len(emeans)):
                # print "  adding emeans[", i, "] = ", emeans[i], " to means[", base_t+i, "]."
                 try:
+                    print len(means), len(variances), base_t, i, base_t+1
                     means[base_t +i] = means[base_t+i] + emeans[i]
                     variances[base_t +i] = variances[base_t+i] + evars[i]
                 except IndexError:
-                    sys.stderr.write("Predicted arrival at time " + str(event[3] + ttime) + " falls outside trace boundary.\n")
+                    sys.stderr.write("Predicted arrival at time " + str(event[3] + ttime) + " - " + str(event[3] + ttime + i*samprate) + " falls outside trace boundaries " + str(stats['starttime'].getTimeStamp()) + " - " + str(stats['starttime'].getTimeStamp() + len(means)*samprate) + ".\n")
                     break
         return (means, variances)
 
@@ -104,24 +104,143 @@ class SigModel:
         plt.ylabel("Envelope Variance")
 
 
+    def waveform_model_trace(self, events, trace, ttimes):
+        (means, variances) = self.all_envelopes(trace.stats, events, ttimes)
+        #self.plot_envelope(trace, events, means, variances)
+
+        ll=0
+        for i in range(len(means)):
+            ll = ll - .5 * np.log(2*np.pi * variances[i]) - .5 * (trace.data[i] - means[i])**2 / variances[i]
+            
+        return ll
+
     def waveform_model(self, events, ttimes):
         """
         Compute p(signals | events, travel times). Combined with the travel time model, we can then integrate over travel times to get p(signals | events), i.e. the overall likelihood.
+
+        The current implementation is a toy model, in which Gaussian
+        white noise is added to a deterministic envelope.
         """
 
+        ll = 0
+        for (i, trace) in enumerate(self.traces):
+            ll = ll + self.waveform_model_trace(events, trace, ttimes[i])
+            
+
+        return ll
+
+
+    def ttime_model_point(self, lat, lon, depth, siteid, phase):
+        return self.earthmodel.ArrivalTime(lat, lon, depth, 0, phase, siteid)
+
+    def ttime_model_logprob(self, ttime, lat, lon, depth, siteid, phase):
+        """
+        Compute p(ttime to siteid | event at lat, lon, depth, etime). 
+        """
+
+        pred_ttime = self.earthmodel.ArrivalTime(lat, lon, depth, 0, phase, siteid)
+        return self.netmodel.arrtime_logprob(ttime, pred_ttime, 0, siteid, phase)
+
     def log_likelihood(self, events):
+        """ 
+        Compute p(signals | events) by integrating over travel times:
+
+        p(signals | events) = int_ttimes p(signals | ttimes, events) p(ttimes | events)
+                            = int_ttimes prod_signal prod_event p(signal | ttime, event)p(ttime | event)
+                            = prod_signal prod_event int_ttimes p(signal | ttime, event)p(ttime | event)
+        """
+
+        TT_SEARCH_STEP = .5 # seconds
+        
+        # TODO: make this depend on some probability threshold, rather than absolute time
+        TT_SEARCH_WIDTH = 10 # seconds
+        MAX_TT = 2000 # seconds
+        MAX_ENVELOPE_LEN = 100 # seconds
+        SUBTRACE_LEN = 2000.0 # seconds
+        
 
         ll = 0
         for trace in self.traces:
+            trace_start = trace.stats['starttime'].getTimeStamp()
+            siteid = int(self.siteid[trace.stats["station"]])
 
-            (means, variances) = self.all_envelopes(trace.stats, events)
+            nsubtraces = np.ceil(len(trace.data)/SUBTRACE_LEN)
+            subtraces = np.array_split(trace.data, nsubtraces)
 
-            for i in range(len(means)):
-                ll = ll - .5 * np.log(2*np.pi * variances[i]) - .5 * (trace.data[i] - means[i])**2 / variances[i]
+            trace_ll = 0
 
-            self.plot_envelope(trace, events, means, variances)
+            # compute loglikelihood for each subtrace
+            for (i, subtrace_data) in enumerate(subtraces):
+                subtrace_start = trace_start + i * SUBTRACE_LEN
+                subtrace_end = subtrace_start + len(subtrace_data)
+                
+                subtrace_ll = 0
+
+                subtrace = self.__copy_trace__(trace, subtrace_data)
+
+                relevant_events = []
+                arrtimess = []
+                for event in events:
+                    etime = event[3]
+                    lat = event[0]
+                    lon = event[1]
+                    depth = event[2]
+                    point_arrtime = etime + self.ttime_model_point(lat, lon, depth, siteid, 0)
+                    min_arrtime = point_arrtime - TT_SEARCH_WIDTH/2
+                    max_arrtime = point_arrtime + TT_SEARCH_WIDTH/2
+
+                    # don't consider events that can't possibly affect the current subtrace
+                    if max_arrtime + MAX_ENVELOPE_LEN < subtrace_start or min_arrtime > subtrace_end:
+                        continue
+
+                    arrtimes = np.arange(min_arrtime, max_arrtime, TT_SEARCH_STEP)
+                    relevant_events.append(event)
+                    print "building artimess: appending ", arrtimes
+                    arrtimess.append(arrtimes)
+
+                total_combos = int((max_arrtime - min_arrtime) ** len(relevant_events))
+                print "looping over ", total_combos, " total combos on subtrace ", i, "."
+                for i in range(total_combos):
+                    indices = self.__decode_counter__(i, max_arrtime - min_arrtime)
+                    ttimes = []
+                    ttll = 0
+                    for (i, idx) in enumerate(indices):
+                        ttimes.append(arrtimess[i][idx]-relevant_events[i][3])
+                        ttll = ttll + self.ttime_model_logprob(ttimes[-1], lat, lon, depth, siteid, 0)
+                    print "testing atimes ", ttimes, " with lp ", ttll
+                    wavell = self.waveform_model_trace(relevant_events, subtrace, ttimes)
+
+                    # equiv to p() = p() + p(subtrace | ttimes, events)p(ttimes|events)
+                    # which will eventually sum over all choices of travel times.
+                    subtrace_ll = self.__log_addprob__(subtrace_ll, ttll + wavell)
+                    print "added subtrace_ll now ", subtrace_ll
+                trace_ll = trace_ll + subtrace_ll
+                print "added trace_ll now ", trace_ll
+            ll = ll + trace_ll
+            print "added ll now ", ll
         return ll
 
+    #TODO: move this and following to a util package
+    def __log_addprob__(self, lp1, lp2):
+        a = np.max((lp1, lp2))
+        b = np.min((lp1, lp2))
+        if a-b > 20:
+            return a
+        else:
+            return a + np.log(1 + np.exp(a-b))
+
+    def __decode_counter__(self, c, base):
+        counts = []
+        while c > 0:
+            counts.append(np.mod(c, base))
+            c = int(np.floor(c/base))
+        return counts
+        
+    def __copy_trace__(self, trace, newdata):
+        new_header = trace.stats.copy()
+        new_header["npts_processed"] = len(newdata)
+        processed_trace = Trace(newdata, header=new_header)
+        return processed_trace    
 
 def window_energies(trace, window_size=1, overlap=0.5):
   """
