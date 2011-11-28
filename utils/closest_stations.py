549708b44cb49c6dfa05ba5e4f692991fb17adb4
David Moore <davmre@gmail.com>
2011-11-28 14:15:26 -0800

    closest_stations now takes an event as command-line arg

--- utils/closest_stations.py
+++ utils/closest_stations.py
@@ -1,10 +1,20 @@
 import database.db
 from database.dataset import *
 import utils.geog
+import sys
+
+if len(sys.argv) < 2:
+    print "not enough arguments (need leb event ID): ", sys.argv
+    sys.exit(1)
 
 cursor = database.db.connect().cursor()
 sites = read_sites(cursor)
-ssites = utils.geog.stations_by_distance(-10,-10,sites)
+cursor.execute("select lon, lat, depth, time, mb, orid from leb_origin "
+                   "where orid=%s" % (sys.argv[1]))
+ev = cursor.fetchone()
+print ev
+print "occurs", (float(ev[EV_TIME_COL]) - 1237680000)/3600.0, "hours in."
+ssites = utils.geog.stations_by_distance(ev[EV_LON_COL],ev[EV_LAT_COL],sites)
 
 for site in ssites:
-    print site
+    print site[0]+1, site[1][0]
