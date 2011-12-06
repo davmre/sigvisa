07ec2883c771f8e28f092232875a4f9294fcf1aa
David Moore <dmoore@cs.berkeley.edu>
2011-12-06 01:42:15 -0800

    cleaner treatment of indices

--- utils/arrival_likelihood.py
+++ utils/arrival_likelihood.py
@@ -21,8 +21,8 @@ cursor = database.db.connect().cursor()
 cursor.execute("select ss.id, ar.time, ar.amp, ar.azimuth, ar.slow from leb_arrival ar, static_siteid ss where arid=%d and ss.sta=ar.sta" % (arid))
 arr = np.array(cursor.fetchall())[0]
 
-start_time = arr[1] - 5
-end_time = start_time + 35
+start_time = arr[1] - 10
+end_time = start_time + 40
 
 sites = read_sites(cursor)
 site_up = read_uptime(cursor, start_time, end_time)
@@ -38,20 +38,35 @@ earthmodel = learn.load_earth("parameters", sites, phasenames, phasetimedef)
 sigmodel = learn.load_sigvisa("parameters", start_time, end_time, ar_perturb, 
                               site_up, sites, phasenames,
                               phasetimedef)
-
-energies, traces = sigvisa_util.load_and_process_traces(cursor, start_time, end_time, window_size=1, overlap=0.5, stalist=(int(arr[0]),))
+stalist=(int(arr[0]),)
+print stalist
+energies, traces = sigvisa_util.load_and_process_traces(cursor, start_time, end_time, window_size=1, overlap=0.5, stalist=stalist)
 sigmodel.set_waves(traces)
 sigmodel.set_signals(energies)
 
-for t in range(0,10):
+arrll = [0 for i in range(0,40)]
+for i in range(0,40):
+    t = float(i)/2
     atime = start_time + t
-    arrll = sigmodel.arrival_likelihood(atime, arr[2], arr[3], arr[4], 0)
-    print "ll of start_time", atime, "is", arrll
+    arrll[i] = sigmodel.arrival_likelihood(atime, arr[2], arr[3], arr[4], 0)
 
-#pp = PdfPages('logs/sample_ar.pdf')
-#for signal in signals:
-#    print signal
-#    title = "orid " + str(orid) + " station " + str(signal[0].stats['siteid']) + " times " + str(start_time) + " " #+ str(end_time)
-#    utils.waveform.plot_segment(signal, title=title)
-#    pp.savefig()
-#pp.close()
+bestt = 0
+besttll = float("-inf")
+for i in range(0,40):
+    t = float(i)/2
+    atime = start_time + t
+    print "ll of start_time", atime, "is", arrll[i]
+    if arrll[i] > besttll:
+        bestt = t-10
+        besttll = arrll[i]
+
+print "logging ", bestt, besttll, sigmodel.arrival_likelihood(start_time+bestt, arr[2], arr[3], arr[4], 1)
+
+pp = PdfPages('logs/ar_likelihood.pdf')
+x =  np.arange(-10, 10,0.5)
+plt.figure()
+plt.xlabel("Time (s)")
+plt.ylabel("Log-likelihood")
+plt.plot(x, arrll[0:40], 'b-')
+pp.savefig()
+pp.close()
