f6513b3461d1eeea88a60d1fba067c208e48ba51
David Moore <davmre@gmail.com>
2012-02-07 02:42:28 -0800

    oops, forgot plotting code

---
+++ plot.py
@@ -0,0 +1,116 @@
+import matplotlib.pyplot as plt
+
+from obspy.core import Trace, Stream, UTCDateTime
+
+from database.dataset import *
+import database.db
+import learn, netvisa
+from utils.geog import dist_deg, azimuth
+
+
+
+# does not save for you - you need to call savefig() yourself!
+def plot_segment(channel_traces, title=None, all_det_times=None, format = "k:"):
+  plt.figure()
+  plt.xlabel("Time (s)")
+
+  for chidx, trc in enumerate(channel_traces):
+    if chidx == 0:
+      axes = plt.subplot(len(channel_traces), 1, 1)
+      if title is not None:
+        plt.title(title)
+    else:
+      plt.subplot(len(channel_traces), 1, chidx+1, sharex=axes)
+      
+    chan_name = trc.stats["channel"]
+
+    plt.ylabel(chan_name)
+
+    if "window_size" in trc.stats:
+      srate = 1/ ( trc.stats.window_size * (1- trc.stats.overlap) )
+      npts = trc.stats.npts_processed
+    else:
+      srate = trc.stats.sampling_rate
+      npts = trc.stats.npts
+    stime = trc.stats["starttime_unix"]
+    timevals = np.arange(stime, stime + npts/srate, 1.0 /srate)[0:npts]
+
+    plt.plot(timevals, trc, format)
+
+    if all_det_times is not None:
+      maxtrc, mintrc = float(max(trc.data)), float(min(trc.data))
+      plt.bar(left=all_det_times, height=[maxtrc-mintrc for _ in all_det_times],
+              width=.25, bottom=mintrc, color="red", linewidth=0, alpha=.5)
+
+def plot_trace(trc, title=None, all_det_times=None, format="k:"):
+  plt.figure()
+  plt.xlabel("Time (s)")
+
+  if title is not None:
+    plt.title(title)
+      
+  chan_name = trc.stats["channel"]
+
+  plt.ylabel(chan_name)
+
+  if trc.stats["window_size"] is not None:
+    srate = 1/ ( trc.stats.window_size * (1- trc.stats.overlap) )
+    npts = trc.stats.npts_processed
+  else:
+    srate = trc.stats.sampling_rate
+    npts = trc.stats.npts
+  stime = trc.stats["starttime_unix"]
+  timevals = np.arange(stime, stime + npts/srate, 1.0 /srate)
+
+  plt.plot(timevals, trc, format)
+
+  if all_det_times is not None:
+    maxtrc, mintrc = float(max(trc.data)), float(min(trc.data))
+    plt.bar(left=all_det_times, height=[maxtrc-mintrc for _ in all_det_times],
+            width=.25, bottom=mintrc, color="red", linewidth=0, alpha=.5)
+
+
+
+# does not save for you - you need to call savefig() yourself!
+def plot_bands(bands_dict, title=None, all_det_times=None):
+  format = "k-"
+    
+  plt.figure(figsize=(12, 30))
+  plt.xlabel("Time (s)")
+
+  for (bidx, band) in enumerate(sorted(bands_dict.keys())):
+    if bidx == 0:
+      axes = plt.subplot(len(bands_dict), 1, 1)
+      if title is not None:
+        plt.title(title)
+    else:
+      plt.subplot(len(bands_dict), 1, bidx+1, sharex=axes)
+      
+    if band.startswith("narrow_logenvelope_"):
+        yl = band[19:]
+    else:
+        yl = band
+    plt.ylabel(yl)
+
+    trc = bands_dict[band]
+    npts = trc.stats["npts"]
+    srate = trc.stats["sampling_rate"]
+    stime = trc.stats["starttime_unix"]
+    timevals = np.arange(stime, stime + npts/srate, 1.0 /srate)[0:npts]
+
+    
+    for (i, n) in enumerate(trc.data):
+        if np.isnan(n) or not np.isfinite(n):
+            trc.data[i] = 0
+
+#    if np.isnan(s) or not np.isfinite(s):
+#        print "invalid data in band %s, not plotting..." % (band)
+#        continue
+
+    plt.plot(timevals, trc, format)
+
+    if all_det_times is not None:
+      maxtrc, mintrc = float(max(trc.data)), float(min(trc.data))
+      plt.bar(left=all_det_times, height=[maxtrc-mintrc for _ in all_det_times],
+              width=.25, bottom=mintrc, color="red", linewidth=0, alpha=.5)
+
