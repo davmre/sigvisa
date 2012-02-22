2785f29031ac6f6cd740c44b061479be19a60513
nimar.arora <nimar.arora@334f63d7-9462-3ba8-66c2-e6a200314d38>
2012-02-22 03:13:05 +0000

    hierarchical arrival time, azimuth, and slowness
    git-svn-id: http://netvisa.googlecode.com/svn/trunk@207 334f63d7-9462-3ba8-66c2-e6a200314d38

--- priors/ArrivalTimePrior.py
+++ priors/ArrivalTimePrior.py
@@ -3,21 +3,11 @@ from utils import Laplace
 from database.dataset import EV_LON_COL, EV_LAT_COL, EV_DEPTH_COL,\
      EV_TIME_COL, DET_SITE_COL, DET_TIME_COL
 
-NUM_PRIOR = 10                          # number of prior points
-
 def learn(param_filename, earthmodel, detections, leb_events, leb_evlist):
-  overall_res = []
-  
-  # keep a list of arrival time residuals for each site
-  site_res = dict((siteid, []) for siteid in range(earthmodel.NumSites()))
-  
-  # keep residuals for each phase
-  phase_res = dict((phaseid, []) for phaseid in range(earthmodel.NumPhases()))
-  
   # keep a residual for each site and phase
-  site_phase_res = dict(((siteid, phaseid), [])
-                        for phaseid in range(earthmodel.NumPhases())
-                        for siteid in range(earthmodel.NumSites()))
+  phase_site_res = [[[] for siteid in range(earthmodel.NumSites())]
+                        for phaseid in range(earthmodel.NumTimeDefPhases())]
+                        
   
   for evnum, event in enumerate(leb_events):
     for phaseid, detnum in leb_evlist[evnum]:
@@ -33,61 +23,29 @@ def learn(param_filename, earthmodel, detections, leb_events, leb_evlist):
         continue
       
       res = arrtime - pred_arrtime
-
+      
       if res > 1000:
         raise ValueError("residual too large")
       
-      overall_res.append(res)
-      site_res[siteid].append(res)
-      phase_res[phaseid].append(res)
-      site_phase_res[siteid, phaseid].append(res)
-
-  # learn the overall residual for and sample some points
-  # for an empirical Bayes prior
-  overall_loc, overall_scale = Laplace.estimate(overall_res)
-  overall_prior = [Laplace.sample(overall_loc, overall_scale) for _ in
-                   xrange(NUM_PRIOR)]
-
-  print "Overall Arrival Time location and scale:", overall_loc, overall_scale
-
-  # learn the overall site residual for each site and sample some points
-  # for an empirical Bayes prior
-  site_prior = {}
-  print "Site Arrival Time Residuals: Location and Scale"
-  for siteid in range(earthmodel.NumSites()):
-    reslist = site_res[siteid]
-    if len(reslist) < NUM_PRIOR:
-      site_prior[siteid] = []
-    else:
-      site_loc, site_scale = Laplace.estimate(reslist)
-      site_prior[siteid] = [Laplace.sample(site_loc, site_scale)
-                            for _ in xrange(NUM_PRIOR)]
-      print siteid, ":", site_loc, site_scale
+      phase_site_res[phaseid][siteid].append(res)
   
-  # learn the overall phase residual for each phase and sample some points
-  # for an empirical Bayes prior
-  phase_prior = {}
-  print "Phase Arrival Time Residuals: Location and Scale"
-  for phaseid in range(earthmodel.NumPhases()):
-    reslist = phase_res[phaseid]
-    if len(reslist) < NUM_PRIOR:
-      phase_prior[phaseid] = []
-    else:
-      phase_loc, phase_scale = Laplace.estimate(reslist)
-      phase_prior[phaseid] = [Laplace.sample(phase_loc, phase_scale)
-                              for _ in xrange(NUM_PRIOR)]
-      print phaseid, ":", phase_loc, phase_scale
+  phase_site_param = []
+  # for each phase
+  print "Arrival Time:"
+  for phaseid in range(earthmodel.NumTimeDefPhases()):
+    # we will learn each site's location and scale
+    print earthmodel.PhaseName(phaseid),":"
+    site_param, loc, scale, beta=Laplace.hier_estimate(phase_site_res[phaseid])
+    print loc, scale, beta
+    phase_site_param.append(site_param)
   
   fp = open(param_filename, "w")
   
-  print >>fp, earthmodel.NumSites(), earthmodel.NumPhases()
+  print >>fp, earthmodel.NumSites(), earthmodel.NumTimeDefPhases()
   
   for siteid in range(earthmodel.NumSites()):
-    for phaseid in range(earthmodel.NumPhases()):
-      reslist = site_phase_res[siteid, phaseid] + site_prior[siteid]\
-                + phase_prior[phaseid] + overall_prior
-      loc, scale = Laplace.estimate(reslist)
+    for phaseid in range(earthmodel.NumTimeDefPhases()):
+      loc, scale = phase_site_param[phaseid][siteid]
       print >>fp, loc, scale
-      
-  fp.close()
   
+  fp.close()
--- utils/Laplace.py
+++ utils/Laplace.py
@@ -119,3 +119,65 @@ def estimate_trunc(vals):
   return location, scale, minval, maxval
 
 
+# site_data is a list of data x_k^i, i=1..n_k for k=1..K sites
+#
+# x_k^i        ~ Laplace(loc_k, scale_k)
+# loc_k        ~ Laplace(loc, scale)
+# loc          ~ Laplace(loc0, scale0)
+# scale^{-1}   ~ Gamma(alpha0, beta0)
+# scale_k^{-1} ~ Gamma(alpha1, beta)
+# beta^{-1}    ~ Gamma(alpha2, beta2)
+#
+# We use the following update rules
+# loc_k = wt-median(x_k^i : scale_k i=1..n_k, loc : scale)
+# loc = wt-median(loc_k : scale k=1..K, loc0 : scale0)
+# scale = \frac{\sum_{k=1..K} |loc_k-loc| + 1/beta0} {K + alpha0 - 1}
+# scale_k = \frac{\sum_{i=1..n_k} |x_k^i - loc_k| + 1/beta} {n_k + alpha1 - 1}
+# beta = \frac{\sum_{k=1..K}1/scale_k + 1/beta2 } {K alpha1 + alpha2 - 1}
+#
+def hier_estimate(site_data, init_loc=0., init_scale=1.,
+                  loc0=0., scale0=100., alpha0=1e-2, beta0=1e2,
+                  alpha1=1., alpha2=1e-2, beta2=1e-2):
+  
+  K = len(site_data)
+  assert K > 1                    # there should be more than one site
+  
+  loc, scale = init_loc, init_scale
+  
+  site_params = np.ndarray((K, 2))
+  site_params[:, 0] = init_loc
+  site_params[:, 1] = init_scale
+  
+  beta = ((1.0/site_params[:,1]).sum() + (1./beta2))/(K*alpha1 + alpha2 - 1.)
+  
+  while True:
+    prev_loc, prev_scale, prev_beta = loc, scale, beta
+
+    # update the location and scale for each site
+    for siteid, data in enumerate(site_data):
+      if not len(data):
+        this_loc = loc
+        if alpha1 > 1:
+          this_scale = 1./(beta * (alpha1 - 1))
+        else:
+          # the gamma distribution has a mode at 0 so instead we pick
+          # the mean in this case
+          this_scale = 1./(beta * alpha1)
+      
+      else:
+        this_loc = np.median(data)
+        this_scale = (sum(abs(x-this_loc) for x in data) + (1./beta)) \
+                     / (len(data) + alpha1 - 1)
+        
+      site_params[siteid] = (this_loc, this_scale)
+
+    # update global location, scale, and beta
+    loc = np.median(site_params[:,0])
+    scale = ((abs(site_params[:,0] - loc)).sum() +(1./beta0))/(K + alpha0 - 1.)
+    beta = ((1.0/site_params[:,1]).sum() + (1./beta2))/(K*alpha1 + alpha2 - 1.)
+    
+    if abs(prev_loc - loc) < .01 and abs(prev_scale - scale) < .01\
+       and abs(prev_beta - beta) < .01:
+      break
+
+  return site_params, loc, scale, beta
