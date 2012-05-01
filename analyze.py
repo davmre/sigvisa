c08ad94048bf86778d06120f38f2898d11cd857a
nimar.arora <nimar.arora@334f63d7-9462-3ba8-66c2-e6a200314d38>
2012-05-01 19:02:44 +0000

    add a license file
    git-svn-id: http://netvisa.googlecode.com/svn/trunk@213 334f63d7-9462-3ba8-66c2-e6a200314d38

--- analyze.py
+++ analyze.py
@@ -1,3 +1,30 @@
+# Copyright (c) 2012, Bayesian Logic, Inc.
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of Bayesian Logic, Inc. nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+# Bayesian Logic, Inc. BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+# 
 # interpret the results of a run
 import os, sys
 import numpy as np
@@ -293,7 +320,10 @@ def gui(options, leb_events, sel3_events, events):
   plt.legend(loc = "upper right")
   plt.grid(True)
   if options.write:
-    plt.savefig("output/run_%d_roc.png" % (options.runid))
+    if options.type1:
+      plt.savefig("output/run-%d-pr.pdf" % (options.runid))
+    else:
+      plt.savefig("output/run-%d-pr.png" % (options.runid))
   
   plt.show()
 
