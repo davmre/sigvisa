428eae9d200d71158661af7f517bfd338bf2ff5a
David Moore <davmre@gmail.com>
2012-05-13 23:05:58 -0700

    nnetvisa.[ch]

--- netvisa.h
+++ netvisa.h
@@ -1,24 +1,145 @@
-#ifndef NETVISA_INCLUDE
-#define NETVISA_INCLUDE
-
+/*
+ * Copyright (c) 2012, Bayesian Logic, Inc.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Bayesian Logic, Inc. nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * Bayesian Logic, Inc. BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * 
+ */
 #include <Python.h>
 /* we don't want _import_array defined in all files except the main
  * module of netvisa */
 #if !defined NETVISA_MAIN_MODULE
   #define NO_IMPORT_ARRAY
 #endif
-
-
 #define PY_ARRAY_UNIQUE_SYMBOL PyArray_API
 #include "numpy/arrayobject.h"
+
+/* constants */
+#define MIN_MAGNITUDE   ((double) 2.0)
+#define MAX_MAGNITUDE   ((double) 8.0)
+
+#define MIN_DEPTH       ((double) 0.0)
+#define MAX_DEPTH       ((double) 700.0)
+
+#define MIN_SLOWNESS    ((double) 0.0)
+#define MAX_SLOWNESS    ((double) 400.0)
+
+#define LOGPROB_UNIFORM_SLOWNESS (- log(MAX_SLOWNESS - MIN_SLOWNESS))
+
+#define MIN_AZIMUTH     ((double) -180)
+#define MAX_AZIMUTH     ((double) +180)
+
+#define LOGPROB_UNIFORM_AZIMUTH (- log(MAX_AZIMUTH - MIN_AZIMUTH))
+
+#define PHASENAME_MAXLEN 6
+
+/* maximum time taken by any phase */
+#define MAX_TRAVEL_TIME ((double) 2000.0)
+
+/* maximum number of primary + secondary detections for any phase */
+#define MAX_PHASE_DET  15
+
+/* max time residual should not be needed in a proper model but due to
+ * unexplained extremely high variance in some of the travel time residual
+ * we need to put this in place for now */
+#define MAX_TIME_RESIDUAL ((double) 6.0)
+
+#define PI                     ((double) 3.1415926535897931)
+#define DEG2RAD                ((double) (PI / 180))
+#define RAD2DEG                ((double) (180 / PI))
+#define AVG_EARTH_RADIUS_KM    ((double) 6371) /* when modeled as a sphere */
+
+/* DELTA_TIME and DELTA_DIST are used in evaluating the answer */
+#define DELTA_TIME 50                        /* in seconds */
+#define DELTA_DIST 5                         /* in degrees */
+
+#define MIN_AMP 0.01
+#define LOG_MIN_AMP -4.6052
+#define LOG10_MIN_AMP -2
+#define MAX_AMP 10000.0
+#define LOG_MAX_AMP 9.2103404
+#define LOG10_MAX_AMP 4.0
+
+typedef struct Event_t
+{
+  double evlon;
+  double evlat;
+  double evdepth;
+  double evtime;
+  double evmag;
+
+  /* array of numsites * numtimedefphases */
+  int * p_num_dets; /* number of secondary detections, 0 to MAX_PHASE_DET */
+
+  /* array of numsites * numtimedefphases * MAX_PHASE_DET */
+  int * p_all_detids;       /* all detids, first one is the primary */
+  
+  int orid;
+  double evscore;
+} Event_t;
+
+typedef struct Site_t
+{
+  double sitelon;
+  double sitelat;
+  double siteelev;
+  int    siteisarr;
+} Site_t;
+
+typedef struct Detection_t
+{
+  int site_det;
+  int arid_det;
+  double time_det;
+  double deltim_det;
+  double azi_det;
+  double delaz_det;
+  double slo_det;
+  double delslo_det;
+  double snr_det;
+  int phase_det;
+  double amp_det;
+  double per_det;
+
+  int prev_det;               /* the previous detection or -1 */
+  double logprob_det;   /* the log probability of either being noise or coda */
+  
+} Detection_t;
+
+
+#include "priors/SecDetPrior.h"
 #include "priors/NumEventPrior.h"
 #include "priors/EventLocationPrior.h"
 #include "priors/EventMagPrior.h"
 #include "priors/EventDetectionPrior.h"
 #include "priors/ArrivalTimePrior.h"
+#include "priors/NumFalseDetPrior.h"
 #include "priors/ArrivalAzimuthPrior.h"
 #include "priors/ArrivalSlownessPrior.h"
 #include "priors/ArrivalPhasePrior.h"
+#include "priors/ArrivalSNRPrior.h"
 #include "priors/ArrivalAmplitudePrior.h"
 #include "priors/EarthModel.h"
 #include "priors/Laplace.h"
@@ -27,14 +148,16 @@
 #include "priors/LogNormal.h"
 #include "priors/Gamma.h"
 
-#include "priors/SecDetPrior.h"
-#include "priors/NumFalseDetPrior.h"
-#include "priors/ArrivalSNRPrior.h"
-
 typedef struct NetModel_t
 {
   PyObject_HEAD
 
+
+  /* the temperature determines how strongly the deterministic
+  * constraints are enforced. As temperature goes to 0 all constraints
+  * are fully enforced */
+  double temperature;
+  
   double start_time;
   double end_time;
   
@@ -42,13 +165,15 @@ typedef struct NetModel_t
   struct Detection_t * p_detections;
 
   int numsites;
-  int numtime;       /* number of quantized time slots in p_site_up */
-  
-  int * p_site_up;                           /* numsites x numtime */
+  int num_uptime;    /* number of quantized time slots in p_site_up */
+
+  int enable_sec_arr;                  /* enable secondary arrivals */
+
+  int * p_site_up;                         /* numsites x num_uptime */
 
   EarthModel_t * p_earth;
   
-  SecDetPrior_t secdet_prior;
+  SecDetPrior_t sec_det_prior;
   NumEventPrior_t num_event_prior;
   EventLocationPrior_t event_location_prior;
   EventMagPrior_t event_mag_prior;
@@ -63,14 +188,38 @@ typedef struct NetModel_t
   
 } NetModel_t;
 
+#include "priors/score.h"
+#include "infer/infer.h"
+#include "infer/propose.h"
+
 #define UPTIME_QUANT     3600                /* 1 hour */
 
 #define NetModel_IsSiteUp(p_netmodel, siteid, arrtime)\
-  ((arrtime >= (p_netmodel)->start_time) && (arrtime < (p_netmodel)->end_time)\
-   && (p_netmodel)->p_site_up[(siteid) * (p_netmodel)->numtime\
+  ((arrtime >= (p_netmodel)->start_time) &&                     \
+   (arrtime < ((p_netmodel)->end_time + MAX_TRAVEL_TIME))       \
+   && (p_netmodel)->p_site_up[(siteid) * (p_netmodel)->num_uptime\
    + ((int) floor(((arrtime) - (p_netmodel)->start_time) / UPTIME_QUANT))])
 
+#define PI                 ((double) 3.1415926535897931)
 
+#define LAT2Z(lat) (sin((lat) * PI / 180.0))
+#define Z2LAT(z) (asin(z) * 180.0 / PI)
+
+/* event array columns */
+#define EV_LON_COL   0
+#define EV_LAT_COL   1
+#define EV_DEPTH_COL 2
+#define EV_TIME_COL  3
+#define EV_MB_COL    4
+#define EV_ORID_COL  5
+#define EV_NUM_COLS  6
+
+/* site array columns */
+#define SITE_LON_COL      0
+#define SITE_LAT_COL      1 
+#define SITE_ELEV_COL     2
+#define SITE_ISARR_COL    3        /* is the site an array station? */
+#define SITE_NUM_COLS     4
 
 /* detections array columns */
 #define DET_SITE_COL    0
@@ -87,21 +236,37 @@ typedef struct NetModel_t
 #define DET_PER_COL    11
 #define DET_NUM_COLS   12
 
-/* the spacing between secondary detections */
-#define SECDET_INTERVAL ((double) 5.0)
+/* MACROS */
+#define Event2R3Vector(event, vector) do {\
+(vector)[0] = (event)->evlon; (vector)[1] = (event)->evlat;\
+(vector)[2] = (event)->evdepth;} while(0)
 
-/* maximum number of primary + secondary detections for any phase */
-#define MAX_PHASE_DET  15
+#define ARRAY2(arr,i,j) (*((double *)PyArray_GETPTR2(arr,i,j)))
+#define ARRAY1(arr,i) (*((double *)PyArray_GETPTR1(arr,i)))
 
-/* max time residual should not be needed in a proper model but due to
- * unexplained extremely high variance in some of the travel time residual
- * we need to put this in place for now */
-#define MAX_TIME_RESIDUAL ((double) 6.0)
+#define BOOLARRAY2(arr,i,j) (*((npy_bool *)PyArray_GETPTR2(arr,i,j)))
+#define BOOLARRAY1(arr,i) (*((npy_bool *)PyArray_GETPTR1(arr,i)))
 
 
-Event_t * alloc_event_net(NetModel_t * p_netmodel);
-void copy_event_net(NetModel_t * p_netmodel, Event_t * p_tgt_event,
+#define MIN(a,b) ((a) <= (b) ? (a) : (b))
+#define MAX(a,b) ((a) >= (b) ? (a) : (b))
+
+/* RAND_DOUBLE -> random number between 0 and 1 (exclusive) */
+#define RAND_DOUBLE ( ((double) rand() + 1.0) / ((double) RAND_MAX + 2.0) )
+/* RAND_UNIFORM(a,b) -> random value between a and b */
+#define RAND_UNIFORM(a,b) (((double) (a)) + ((double) ((b)-(a))) * RAND_DOUBLE)
+
+#define LOGSUMEXP(a, b) ((a) > (b) ? ( (a) + log(1 + exp((b)-(a))) )    \
+                         : ((b) + log(1 + exp((a) - (b)))) ) 
+
+Event_t * alloc_event(NetModel_t * p_netmodel);
+void free_event(Event_t * p_event);
+void copy_event(NetModel_t * p_netmodel, Event_t * p_tgt_event,
                 const Event_t * p_src_event);
+void print_event(const Event_t * p_event);
 void print_event_detections(EarthModel_t * p_earth, const Event_t * p_event);
 
-#endif // NETVISA_INCLUDE
+void convert_events_to_pyobj(const EarthModel_t * p_earth,
+                             const Event_t ** pp_events, int numevents,
+                             PyObject ** pp_eventsobj,
+                             PyObject ** pp_evdetlistobj);
