add new test cases verifying correct computation of AR likelihood

signals/armodel/model.py
@@ -35,15 +35,16 @@ class ARModel:
     def lklhood(self, data):
         prob = 0
         for t in range(len(data)):
-            if t >= self.p:
-                expected = self.c
-                for i in range(self.p):
+            expected = self.c
+            for i in range(self.p):
+                if t > i:
                     expected += self.params[i] * data[t-i-1]
-                actual = data[t]
-                error = actual - expected
-                prob += self.em.lklhood(error)
+            actual = data[t]
+            error = actual - expected
+            prob += self.em.lklhood(error)
         # normalize the sum of probability (no dependency on p value)
-        return prob/(len(data)-self.p)*len(data)
+#        return prob/(len(data)-self.p)*len(data)
+        return prob
     
     #given data as argument, 
     def errors(self, data):
@@ -98,4 +99,5 @@ class ErrorModel:
     def lklhood(self, x):
         t1 = np.log(self.std) + 0.5 * np.log(2 * np.pi)
         t2 = 0.5 * np.square((x - self.mean) / self.std)
+
         return -(t1 + t2)

test.py
@@ -3,10 +3,17 @@ import unittest
 import numpy as np
 from obspy.core import Trace, Stream, UTCDateTime
 
+import matplotlib
+matplotlib.use('PDF')
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+import plot
+
 import sigvisa, sigvisa_util, learn
-import priors.SignalPrior
+import signals.armodel.model
 from database.dataset import *
 from database import db
+
 
 
 def gen_random_segments(siteids, length):
@@ -20,10 +27,10 @@ def gen_random_segment(siteid, length):
     for (chan, chanid) in (('BHE', 0), ('BHN', 1), ('BHZ', 2)):
         data = np.random.random((length, 1))
         stats = {'network': 's' + str(siteid), 'station': 's' + str(siteid), 'location': '',
-                 'channel': chan, 'npts': len(data), 'sampling_rate': 5,
+                 'channel': chan, 'band': 'narrow_logenvelope_2.00_3.00', 'npts': len(data), 'sampling_rate': 1,
                  'starttime_unix': 10000, 'siteid': siteid, 'chanid': chanid, 'starttime': UTCDateTime(10000)}
         trace = Trace(data = data, header=stats)        
-        channels[chan] = {"broadband_envelope": trace}
+        channels[chan] = {"narrow_logenvelope_2.00_3.00": trace}
     return channels
 
 class TestPurePythonFunctions(unittest.TestCase):
@@ -69,17 +76,52 @@ class TestCFunctions(unittest.TestCase):
         self.set_default_params()
 
     def set_default_params(self):
-        site_params = {"chan_mean_BHZ": 0, "chan_var_BHZ": 1, "chan_mean_BHE": 0, "chan_var_BHE": 1, "chan_mean_BHN": 0, "chan_var_BHN": 1, "env_p_height": 1, "env_p_onset": 0.8, "env_p_decay": 0.04, "env_s_height": 1, "env_s_onset": 0.8, "env_s_decay": 0.04, "ar_noise_sigma2": 0.05, "ar_coeffs": (0.8,)}    
-        self.params = dict()
-        for siteid in self.test_siteids:
-            self.params[siteid] = site_params.copy()
-        self.sigmodel.set_all_signal_params(self.params)
+        arparams = np.array((1.531985598646, -1.0682484475528535, 1.0396481745808401, -1.3279255479118346, 0.98655767845516618, -0.83922136571517214, 0.76677157354780778, -0.59579319975231027, 0.36945613335446836, -0.17841016307209667))
+        noise_mean = 0
+        noise_var = .001
+        wiggle_var = .001
 
-    def test_arrival_likelihood(self):
-        self.sigmodel.set_signals(gen_random_segments((2,), 1000))
-        ll = self.sigmodel.arrival_likelihood(10500, 8, 360, 10, 0, 2, 0)
+        self.ar_noise_model = signals.armodel.model.ARModel(arparams, signals.armodel.model.ErrorModel(noise_mean, np.sqrt(noise_var)))
+
+        band = sigvisa.canonical_band_num("narrow_logenvelope_2.00_3.00")
+        chan_BHZ = sigvisa.canonical_channel_num("BHZ")
+        chan_BHE = sigvisa.canonical_channel_num("BHE")
+        chan_BHN = sigvisa.canonical_channel_num("BHN")
+
+        for siteid in self.test_siteids:
+            self.sigmodel.set_noise_process(siteid, band, chan_BHZ, noise_mean, noise_var, arparams);
+            self.sigmodel.set_noise_process(siteid, band, chan_BHE, noise_mean, noise_var, arparams);
+            self.sigmodel.set_noise_process(siteid, band, chan_BHN, noise_mean, noise_var, arparams);
+            self.sigmodel.set_wiggle_process(siteid, band, noise_mean, wiggle_var, arparams);
+
+
+    def test_noise_trace_likelihood(self):
+        segments = gen_random_segments((2,), 1000)
+        params = np.array(((100000, 1, 0, 1, 0, -0.02),))
+
+        ll1 = self.sigmodel.trace_likelihood(segments[0]['BHZ']['narrow_logenvelope_2.00_3.00'], [1,], params)
+        ll2 = self.ar_noise_model.lklhood(segments[0]['BHZ']['narrow_logenvelope_2.00_3.00'].data)
+        self.assertAlmostEqual(ll1, ll2, delta=.1)
+        
+    def test_segment_likelihood(self):
+        segments = gen_random_segments((2,), 1000)
+        params = np.array(((10200, 4, 7, 2, 5, -0.02),))
+
+        ll = self.sigmodel.segment_likelihood(segments[0], [1,], params)
+        print "got ll", ll
         self.assertTrue(ll < 0)
         
+    def test_sample_segment(self):
+        params = np.array(((10200, 4, 7, 0, 7, -0.02),))
+        seg_template = self.sigmodel.generate_segment(10000, 11000, 2, 1, [1,], params)
+        seg_sample = self.sigmodel.sample_segment(10000, 11000, 2, 1, [1,], params)
+        
+        pp = PdfPages(os.path.join('logs', "test_segments.pdf"))
+        plot.plot_segment(seg_template, title="generated", band="narrow_logenvelope_2.00_3.00")
+        pp.savefig()
+        plot.plot_segment(seg_sample, title="sampled", band="narrow_logenvelope_2.00_3.00")
+        pp.savefig()
+        pp.close()
 
     # Generate a couple of random signals, pass them to Sigmodel to be
     # converted to C structures, and then try to retrieve them. We