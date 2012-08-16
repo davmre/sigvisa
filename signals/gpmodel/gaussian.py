98d1a9ce329f79c5580b8baffe2a1ac5b8ce7474
MinJoon Seo <seominjoon@gmail.com>
2012-08-16 01:26:43 -0700

    gaussian process test 17 (deterministic algorithm) and test 18

--- signals/gpmodel/gaussian.py
+++ signals/gpmodel/gaussian.py
@@ -12,7 +12,7 @@ self.vs: variance of signal
 self.vn: variance of noise
 """
 class GPModel:
-    def __init__(self, domain, params):
+    def __init__(self, domain, params=None):
         domain = np.array(domain)
         dim = len(domain.shape)
         if dim == 0:
@@ -25,7 +25,12 @@ class GPModel:
             raise Exception("Inputs need to be 2D or less.")
         self.domain = np.transpose(self.X)
         self.n, self.xd = self.domain.shape # n=number of points, xd=in dim
-        self.l, self.vs, self.vn = params
+        if params == None:
+            self.l = 0.8
+            self.vs = 1
+            self.vn = 0.01
+        else:
+            self.l, self.vs, self.vn = params
         self.K = self.covmat(self.X, self.X, same=True)
     
     def covvec(self, x1, x2, same=False):
@@ -150,208 +155,3 @@ class GPLearner:
         for i in range(n):
             pd[i] = single_lklhood(f_bars[i], vars[i], target)
         return pd
-    
-
-"""
-def cov(x1, x2, params, same):
-    len_scale, var_signal, var_noise = params
-    n = np.size(x1,0)
-    M = 1/np.square(len_scale)*np.eye(n)
-    t1 = -0.5*np.dot(np.transpose(x1-x2), np.dot(M, x1-x2))
-    t2 = 0
-    if same:
-        t2 = var_noise
-    return var_signal*np.exp(t1)+t2
-
-def covmat(X1, X2, params, same):
-    n1 = np.size(X1, 1)
-    n2 = np.size(X2, 1)
-    K = np.zeros([n1, n2])
-    for i in range(n1):
-        for j in range(n2):
-            K[i][j] = cov(X1[:,i:i+1], X2[:,j:j+1], params, same and i==j)
-    return K
-"""
-"""
-Gaussian process of d-dimensional input space, n training points, then:
-X is d x n (input space x number of training points)
-y is n x 1 (outputs for n training points)
-var_noise is the variance of the noise
-x is d x 1 test point
-"""
-
-"""
-def gp(X, y, params, xs):
-    assert np.size(X,0) == np.size(xs, 0)  
-    dim = np.size(X,0)
-    
-    # misc. values
-    len_scale, var_signal, var_noise = params
-    n = np.size(X,1) # number of training points
-    K = covmat(X,X, params, True)
-    L = np.linalg.cholesky(K+var_noise*np.eye(n))
-    L_inv = np.linalg.inv(L)
-    L_t_inv = np.linalg.inv(np.transpose(L))
-    a = np.dot(L_t_inv,np.dot(L_inv,y))
-
-    # log(p(y|X)) 
-    logp1 = -0.5*np.dot(np.transpose(y),a)-(n/2)*np.log(2*np.pi)
-    logp2 = np.sum([L[i][i] for i in range(n)])
-    logp = logp1-logp2
-
-    # means and variances of xs
-    m = np.size(xs, 1) # number of test points
-    f_bars = np.zeros(m)
-    vars = np.zeros(m)
-    for i in range(m):
-        x = xs[:,i:i+1]
-        k = covmat(X,x, params, False)
-        f_bar = np.dot(np.transpose(k), a)
-        v = np.dot(L_inv, k)
-        var = cov(x,x, params, True)-np.dot(np.transpose(v),v)
-        f_bars[i] = f_bar
-        vars[i] = var
-    
-    return (f_bars, vars, logp)
-"""
-
-"""
-initially intended for multi-dimensional f_bars, but apparently
-f_bars is always 1D... Still works though.
-"""
-"""
-def lklhood(f_bars, vars, target):
-    dim = f_bars.shape
-    out = np.zeros(dim)
-    
-    # precondition: var > 0
-    def single_lklhood(mean, var, x):
-        c = 1/(np.sqrt(2*np.pi*var))
-        e = -0.5*np.square(x-mean)/var
-        return c*np.exp(e)
-
-    def iterate(dim_i, addr):
-        if dim_i == len(dim):
-            mean = f_bars[tuple(addr)]
-            var = vars[tuple(addr)]
-            if var == 0:
-                out[tuple(addr)] = 0
-            else:
-                out[tuple(addr)] = single_lklhood(mean, var, target)
-        else:
-            for i in range(dim[dim_i]):
-                iterate(dim_i+1, np.append(addr, i))
-    iterate(0,[])
-    return out
-"""
-"""
-X = d x n matrix then
-sample a point from n-dimensional Gaussian distribution
-"""
-"""
-def sample(X, params, mean=0):
-    d = np.size(X,1)
-    C = covmat(X, X, params, True)
-    means = mean*np.ones(d)
-    return np.random.multivariate_normal(means, C)
-
-def heatmap2(X, y, res=100):
-    d,N = np.shape(X)
-    X_t = np.transpose(X)
-    x1s = np.zeros(N)
-    x2s = np.zeros(N)
-    for i in range(N):
-        x1s[i], x2s[i] = X_t[i]
-    table, x1edges, x2edges = np.histogram2d(x1s, x2s, bins=res)
-    extent = [x1edges[0], x1edges[-1], x2edges[0], x2edges[-1]]
-    plt.clf()
-    print table
-    plt.imshow(table, extent=extent, interpolation='none')
-
-def getgrid(x1range, x2range, res1=100, res2=100):
-    x1grid = np.linspace(x1range[0],x1range[1],res1)
-    x2grid = np.linspace(x2range[0],x2range[1],res2)
-    return np.transpose([[x1,x2] for x1 in x1grid for x2 in x2grid])
-
-def heatmap(X, y, params, x1range, x2range, res1=100, res2=100, target=None):
-    x1grid = np.linspace(x1range[0],x1range[1],res1)
-    x2grid = np.linspace(x2range[0],x2range[1],res2)
-    list = [(x1,x2) for x1 in x1grid for x2 in x2grid]
-    x1ind = range(res1)
-    x2ind = range(res2)
-    indices = [(x1,x2) for x1 in x1ind for x2 in x2ind]
-    xs = np.transpose(list)
-    f_bars, vars, logp = gp(X, y, params, xs)
-    
-    result = np.zeros([res1,res2])
-    for i in range(len(indices)):
-        x1,x2 = indices[i]
-        result[x1][x2] = f_bars[i]
-    plt.imshow(result, interpolation='none')
-    plt.show()
-    if target != None:
-        p = lklhood(f_bars,vars, target)
-        ptable = np.zeros([res1,res2])
-        for i in range(len(indices)):
-            x1,x2 = indices[i]
-            ptable[x1][x2] = p[i]
-        plt.imshow(ptable, interpolation='none')
-        plt.show()
-
-len_scale = 0.5
-var_signal = 0.8
-var_noise = 0.01
-params = len_scale, var_signal, var_noise
-"""
-"""
-X = np.array([[0,0,1,1],[0,1,0,1]])
-y = np.transpose(np.array([[0,10,3,2]]))
-"""
-
-"""
-N = 101
-grid = np.linspace(0,1,N)
-X_t = [[x1] for x1 in grid]
-X = np.transpose(X_t)
-y = sample(X, params)
-test_i = int(N*(np.random.rand()))
-testy = np.transpose([y[test_i]])
-testx = np.array([X_t[test_i]])
-trainy = np.transpose([np.append(y[:test_i], y[test_i+1:])])
-trainx = np.array([np.append(X_t[:test_i], X_t[test_i+1:])])
-f_bar, var, logp = gp(trainx, trainy, params, testx)
-
-plt.plot(X[0], y)
-plt.plot(testx, f_bar, 'o')
-plt.show()
-
-N2 = 1001
-grid2 = np.linspace(0,1,N2)
-xs_t = [[x1] for x1 in grid2]
-xs = np.transpose(xs_t)
-f_bars, vars, logp = gp(trainx, trainy, params, xs)
-ps = lklhood(f_bars, vars, testy)
-plt.plot(xs[0], ps)
-plt.plot(testx, ps[test_i*(N2-1)/(N-1)], 'o')
-plt.show()
-"""
-
-"""
-N = 11
-grid = np.linspace(0,1,N)
-X_t = [[x1,x2] for x1 in grid for x2 in grid]
-X = np.transpose(X_t)
-y = sample(X, params)
-test_i = int(N*N*(np.random.rand()))
-testy = np.transpose([y[test_i]])
-testx = np.array([X_t[test_i]])
-trainy = np.transpose([np.append(y[:test_i], y[test_i+1:])])
-trainx = np.array([np.append(X_t[:test_i], X_t[test_i+1:])])
-f_bar, var, logp = gp(trainx, trainy, params, testx)
-
-plt.plot(X[0], y)
-plt.plot(testx, f_bar, 'o')
-plt.show()
-"""
-
-#heatmap(X,y,params,[-1,2],[-1,2],target=1)
--- signals/gpmodel/gptest.py
+++ signals/gpmodel/gptest.py
@@ -1,7 +1,19 @@
 import numpy as np
+import scipy as sp
+import pywt
 import matplotlib.pyplot as plt
-from signals.gpmodel.gaussian import *
-from signals.gpmodel.gridutil import *
+from gaussian import *
+from gridutil import *
+from data import *
+import signals.armodel.learner as lnr
+from matplotlib.backends.backend_pdf import PdfPages
+import database.db
+from database.dataset import *
+import utils.geog
+import sys
+import matplotlib.mlab as mlab
+import wave
+import os
 
 def mpd4test(fmodel, trnmodel, testdom, trndom, i, grid, n):
     margin = 0.2
@@ -20,6 +32,468 @@ def pd4test(fmodel, tmodel, testdom, trndom, i, grid):
     pd = testlnr.lklhood(grid,testrange)
     return pd, fullrange
 
+def gp2d(testevid, feature, index, params, gridmin, gridmax, res=101):
+    events = validevents2(ex=testevid,feature=feature)
+    teste = Event(testevid,feature=feature)
+    input = [teste.lat, teste.lon]
+    output = teste.gpval[index]
+    inputs = inputs2d(events)
+    outputs = gpvals(events, index)
+    gpm = GPModel(inputs,params=params)
+    gpl = GPLearner(gpm, outputs)
+    ingrid = makegrid(gridmin, gridmax, res=res)
+    inlist = grid2list(ingrid,2)
+    pdlist = gpl.lklhood(inlist, output)
+    pdgrid = list2grid(pdlist, [res,res])
+    return pdgrid
+
+def draw2d(pdgrid, gridmin, gridmax, testevid):
+    dr, dc = np.unravel_index(np.argmax(pdgrid),pdgrid.shape)
+    res = len(pdgrid)
+    targetlat = float(dr*(gridmax[0]-gridmin[0]))/(res-1)+gridmin[0]
+    targetlon = float(dc*(gridmax[1]-gridmin[1]))/(res-1)+gridmin[1]
+    plt.axvline(targetlon, color='red')
+    plt.axhline(targetlat, color='red')
+    event = Event(testevid)
+    plt.axvline(event.lon, color='white')
+    plt.axhline(event.lat, color='white')
+    plt.imshow(pdgrid,extent=[gridmin[1],gridmax[1],gridmax[0],gridmin[0]], interpolation='none')
+    predicted = Location(targetlat, targetlon, 0)
+    actual = Location(event.lat, event.lon, 0)
+    return predicted.dist(actual)
+
+# for quickly viewing how feature function transform the real data
+def test18(): 
+    testevid = 4689462
+    compevid = 4686108
+    te = Event(testevid, feature=wave.psd)
+    ce = Event(compevid, feature=wave.psd)
+    plt.plot(te.gpval)
+    plt.plot(ce.gpval)
+    print te.dist(ce)
+    plt.show()
+    
+
+def test17():
+    testevid = 4653310
+    feature = wave.psd # feature function used
+    params = (0.4, 1, 0.1) # length, vs, vn
+    gridmin = [32.5,80] # lat, lon min
+    gridmax = [37.5,85] # lat, lon max
+    os.system("mkdir outputs/%d" %testevid) # saves results to outputs folder
+    
+    pdgrid = np.ones([101,101]) # cumulative prob distribution grid
+    
+    for index in range(20): # 'index' indexes values computed by feature function
+        currpdgrid = gp2d(testevid,feature,index,params,gridmin,gridmax)
+        dist = draw2d(currpdgrid,gridmin,gridmax,testevid) # distance between maxima and actual
+        plt.title("distance = %f" %dist)
+        plt.savefig("outputs/%d/%d.png" %(testevid,index))
+        plt.close()
+        pdgrid *= currpdgrid
+        
+    dist = draw2d(pdgrid,gridmin,gridmax,testevid)
+    plt.title("distance = %f" %dist)
+    plt.savefig("outputs/%d/total.png" %testevid)
+    plt.close()
+
+
+def test16():
+    evid1 = 4686108
+    evid2 = 4689462
+    evid3 = 4650871
+    e1 = Event(evid1)
+    e2 = Event(evid2)
+    e3 = Event(evid3)
+    print e1.dist(e3)
+    def filter(x):
+        cA1, cD1 = pywt.dwt(x,'db1') #cA1 0-10 Hz
+        cA2, cD2 = pywt.dwt(cA1,'db1') #cA2 0-5 Hz
+        cA3, cD3 = pywt.dwt(cA2, 'db1') #cA3 0-2.5Hz
+        cA4, cD4 = pywt.dwt(cA3, 'db1') #cD4 1.25-2.5 Hz
+        cA5, cD5 = pywt.dwt(cD4, 'db1') #cA5 1.25-1.875 Hz
+        return x
+    
+    f1 = filter(e1.x)
+    f2 = filter(e2.x)
+    f3 = filter(e3.x)
+    
+    
+    
+    plt.plot(f1)
+    plt.plot(f2)
+    plt.plot(f3)
+    plt.show()
+            
+    y1 = np.fft.rfft(filter(e1.x))
+    y2 = np.fft.rfft(filter(e2.x))
+    y3 = np.fft.rfft(filter(e3.x))
+
+    
+    def convert(x):
+        for i in range(len(x)):
+            if x[i] < 0:
+                x[i] = 2*np.pi+x[i]
+        return x
+    
+    plt.plot(convert(np.angle(y1))[:100])
+    plt.plot(convert(np.angle(y2))[:100])
+    plt.plot(convert(np.angle(y3))[:100])
+    plt.show()
+    
+    plt.plot(np.abs(y1)[:100])
+    plt.plot(np.abs(y2)[:100])
+    plt.plot(np.abs(y3)[:100])
+    plt.show()
+
+def test15():
+    evid1 = 4686108
+    evid2 = 4689462
+    list = closest2(evid=evid1)
+    evid3 = list[-3][1]
+    evid3 = 4657242
+    e1 = Event(evid1)
+    e2 = Event(evid2)
+    e3 = Event(evid3)
+    y1 = np.fft.rfft(e1.x)
+    y2 = np.fft.rfft(e2.x)
+    y3 = np.fft.rfft(e3.x)
+    
+    
+    t = 150
+    x = np.linspace(0,20,len(y1))
+    plt.plot(x[:t],y1[:t])
+    plt.plot(x[:t],y2[:t])
+    plt.plot(x[:t],y3[:t])
+    plt.show()
+    
+    y1p = []
+    x1p = []
+    y1n = []
+    x1n = []
+    
+    y2p = []
+    x2p = []
+    y2n = []
+    x2n = []
+    
+    def divide(y, xp, yp, xn, yn, limit=None):
+        if limit == None:
+            limit = len(y)
+        for i in range(limit):
+            if y[i] >= 0:
+                yp.append(y[i])
+                xp.append(i)
+            else:
+                yn.append(-y[i])
+                xn.append(i)
+    
+    divide(y1,x1p,y1p,x1n,y1n,100)
+    divide(y2,x2p,y2p,x2n,y2n,100)
+    
+    plt.plot(x1p,y1p)
+    plt.plot(x2p,y2p)
+    plt.show()
+
+def test14():
+    evid1 = 4686108
+    evid2 = 4689462
+    list = closest2(evid=evid1)
+    evid3 = list[-4][1]
+#    evid3 = 4657242
+    e1 = Event(evid1)
+    e2 = Event(evid2)
+    e3 = Event(evid3)
+    
+    plt.plot(e3.x)
+
+    plt.show()
+
+    nf = 512
+    y1, x1= mlab.psd(e1.x,NFFT=nf,Fs=40)
+    y2, x2 = mlab.psd(e2.x,NFFT=nf,Fs=40)
+    y3, x3 = mlab.psd(e3.x,NFFT=nf,Fs=40)
+    t = 100
+    plt.plot(x1,np.log(y1))
+    plt.plot(x2,np.log(y2))
+    plt.plot(x3,np.log(y3))
+    plt.show()
+
+def test13():
+    l = closest2()
+    k = 5
+    fl = l[:k]
+    sl = l[-k:]
+    fl.extend(sl)
+    for x in fl:
+        dist, evid1, evid2 = x
+        print "creating pdf for %d-%d..." %(evid1,evid2)
+        createpdf(evid1, evid2)
+    
+def test12():
+    f = open('closest.txt', 'w')
+    list = closest2()
+    for x in list:
+        f.write("%d, %d, %f\n" %(x[1],x[2], x[0]))
+    f.close()    
+
+def createpdf(evid1, evid2, N=8):
+
+    e1 = Event(evid1)
+    e2 = Event(evid2)
+    name = 'outputs/%f_%d-%d.pdf' %(e1.dist(e2), evid1, evid2)
+    print "creating %s..." %name
+    pdf = PdfPages(name)
+    Fs=40.0
+
+    plt.subplot(411)
+    plt.plot(e1.x)
+    plt.title("Origianl")
+    plt.subplot(412)
+    plt.plot(e2.x)
+    plt.subplot(413)
+    plt.psd(e1.x, Fs=Fs)
+    plt.psd(e2.x, Fs=Fs)
+    plt.subplot(414)
+    plt.plot(np.correlate(e1.x,e2.x,mode='full'))
+    plt.savefig(pdf, format='pdf')
+    plt.close()
+    
+
+    cA1, cD1 = pywt.dwt(e1.x,'db1')
+    cA2, cD2 = pywt.dwt(e2.x,'db1')
+    max = 20.0
+    for i in range(N):  
+        mid = max / 2.0
+        Fs /= 2.0
+        plt.subplot(411)
+        plt.plot(cA1)
+        plt.title("0-%f Hz" %(mid))        
+        plt.subplot(412)
+        plt.plot(cA2)
+        plt.subplot(413)
+        plt.psd(cA1, Fs=Fs)
+        plt.psd(cA2, Fs=Fs)
+        plt.subplot(414)
+        plt.plot(np.correlate(cA1,cA2,mode='full'))
+      
+
+        plt.savefig(pdf, format='pdf')
+        plt.close()
+
+      
+        plt.subplot(411)
+        plt.plot(cD1)
+        plt.title("%f-%f Hz" %(mid, max))          
+        plt.subplot(412)
+        plt.plot(cD2)
+        plt.subplot(413)
+        plt.psd(cD1, Fs=Fs)
+        plt.psd(cD2, Fs=Fs)
+        plt.subplot(414)
+        plt.plot(np.correlate(cD1,cD2,mode='full'))
+        plt.savefig(pdf, format='pdf')
+        plt.close()
+        
+        max /= 2.0
+        
+        cA1, cD1 = pywt.dwt(cA1, 'db1')
+        cA2, cD2 = pywt.dwt(cA2, 'db1')
+        
+    pdf.close()
+
+def test10():
+    evid1 = 4686108
+    evid2 = 4689462
+    evid3 = 4650871
+    e1 = Event(evid1)
+    e2 = Event(evid2)
+    e3 = Event(evid3)
+    x1 = e1.data-np.mean(e1.data)
+    x2 = e2.data-np.mean(e2.data)
+    x3 = e3.data-np.mean(e3.data)
+    plt.plot(e1.data)
+    plt.plot(e2.data)
+    plt.plot(e3.data)
+    plt.show()
+    a=1
+    FS = 40
+    FC = 1.5/(0.5*FS)
+    N = 41
+    b=signal.firwin(N,FC)
+    y1 = signal.lfilter(b,a,x1)
+    y2 = signal.lfilter(b,a,x2)
+    y3 = signal.lfilter(b,a,x3)
+    plt.plot(y1, 'b')
+    plt.plot(y2, 'r')
+    plt.plot(y3, 'g')
+    plt.show()
+
+# returns a res by res matrix containing the probability values
+
+def test9():
+    evid = 4733535
+    events = validevents2(ex=evid)
+    lons = np.zeros(len(events))
+    lats = np.zeros(len(events))
+    p = 5
+    inputs = np.zeros([len(events), 2])
+    outputs_t = np.zeros([p,len(events)])
+    for i in range(len(events)):
+        e = events[i]
+        lons[i] = e.lon
+        lats[i] = e.lat
+        inputs[i] = [e.lat, e.lon]
+        l = lnr.ARLearner(e.data)
+        param,std = l.yulewalker(p)
+        for ind in range(p):
+            outputs_t[ind][i] = param[ind]
+    e = Event(evid, phase=None)
+    input = [e.lat, e.lon]
+    l = lnr.ARLearner(e.data)
+    param, std = l.yulewalker(5)
+    output_t = param
+    
+    l = 0.4
+    vs = 1
+    vn = 0.1
+    params = (l,vs,vn)
+    gpm = GPModel(inputs,params=params)
+    ingrid = makegrid([32.5,80], [37.5,85], res=101)
+    inlist = grid2list(ingrid,2)    
+
+    def pd(outputs, output):
+        gpl = GPLearner(gpm, outputs)
+        return gpl.lklhood(inlist, output)
+
+    margin = 0.3
+    pdlist = pd(outputs_t[0], output_t[0]) + margin
+    for i in range(p-1):
+        pdlist *= pd(outputs_t[i+1], output_t[i+1]) + margin
+
+    pdgrid = list2grid(pdlist, [101,101])
+    print np.unravel_index(np.argmax(pdgrid),pdgrid.shape)
+    plt.axvline(e.lon, color='white')
+    plt.axhline(e.lat, color='white')
+    plt.imshow(pdgrid,extent=[80,85,37.5,32.5], interpolation='none')
+    plt.show()
+
+def test8():
+    k = int(sys.argv[2])
+    evid = 4653310
+    events = validevents2(ex=evid)
+    lons = np.zeros(len(events))
+    lats = np.zeros(len(events))
+    inputs = np.zeros([len(events), 2])
+    outputs = np.zeros(len(events))
+    for i in range(len(events)):
+        e = events[i]
+        lons[i] = e.lon
+        lats[i] = e.lat
+        inputs[i] = [e.lat, e.lon]
+        l = lnr.ARLearner(e.data)
+        param,std = l.yulewalker(5)
+        outputs[i] = param[k]
+        
+    e = Event(evid, phase=None)
+    input = [e.lat, e.lon]
+    l = lnr.ARLearner(e.data)
+    param, std = l.yulewalker(5)
+    output = param[k]
+    
+    l = 0.2
+    vs = 0.5
+    vn = 0.1
+    params = (l,vs,vn)
+    gpm = GPModel(inputs,params=params)
+    gpl = GPLearner(gpm, outputs)
+    ingrid = makegrid([32.5,80], [37.5,85], res=101)
+    inlist = grid2list(ingrid,2)
+    outlist = gpl.lklhood(inlist, output)
+    outgrid = list2grid(outlist, [101,101])
+    plt.axvline(e.lon, color='white')
+    plt.axhline(e.lat, color='white')
+    plt.imshow(outgrid,extent=[80,85,37.5,32.5],interpolation='none')
+    print outputs
+    print e.lon, e.lat
+    
+    plt.show()
+
+def test7():
+    events = validevents("GNI")
+    print len(events)
+    inputs = np.zeros([len(events),2])
+    outputs = np.zeros(len(events))
+    for i in range(len(events)):
+        e = events[i]
+        inputs[i] = [e.lat, e.lon]
+        outputs[i] = e.y
+    ingrid = makegrid([-90,-180],[90,180],res=101)
+    inlist = grid2list(ingrid, 2)
+    gpm = GPModel(inputs)
+    lnr = GPLearner(gpm, outputs)
+    outlist, vars, logp = lnr.predict(inlist)
+    outgrid = list2grid(outlist, [101,101])
+    plt.imshow(outgrid,interpolation='none')
+
+    plt.show()
+
+def test6():
+    evids = validevids("GNI")
+    for evid in evids:
+        e = Event(evid)
+        plt.plot(e.data)
+        plt.show()
+
+def test5():
+    evids = validevids("LPAZ")
+    print len(evids)
+    for evid in evids:
+        try:
+            e = Event(evid)
+            plt.plot(e.lat, e.lon, "o")
+        except:
+            pass
+    plt.show()
+
+def test4():
+    if len(sys.argv) < 3:
+        print "need two evid to compare their wiggles."
+    siteid = "2"
+    phase = "0.70_1.00"
+    evid1 = sys.argv[1]
+    evid2 = sys.argv[2]
+    evid3 = sys.argv[3]
+    evid4 = sys.argv[4]
+    prefix = "../sigvisa_data/wiggles/3/%s/1/%s/" %(siteid, phase)
+    suffix = "_BHZ.dat"
+    addr1 = "%s%s%s" %(prefix, evid1, suffix)
+    addr2 = "%s%s%s" %(prefix, evid2, suffix)
+    addr3 = "%s%s%s" %(prefix, evid3, suffix)
+    addr4 = "%s%s%s" %(prefix, evid4, suffix)
+
+    wiggle1 = np.loadtxt(addr1)
+    wiggle2 = np.loadtxt(addr2)
+    wiggle3 = np.loadtxt(addr3)
+    wiggle4 = np.loadtxt(addr4)
+
+    def plot4(d1, d2, d3, d4):
+        ax1 = plt.subplot(221)
+        plt.plot(d1)
+        plt.title(evid1)
+        ax2 = plt.subplot(223, sharex=ax1, sharey=ax1)
+        plt.plot(d2)
+        plt.title(evid2)
+        ax3 = plt.subplot(222, sharey=ax1, sharex=ax1)
+        plt.plot(d3)
+        plt.title(evid3)
+        ax4 = plt.subplot(224, sharex=ax1, sharey=ax1)
+        plt.plot(d4)
+        plt.title(evid4)
+        plt.show()
+        
+    plot4(wiggle1,wiggle2,wiggle3,wiggle4)
+
 def test3():
     l = 0.8
     vs = 1
@@ -36,7 +510,7 @@ def test3():
     fullmodel = GPModel(fulldomain,params)
     trnmodel = GPModel(trndomain,params)
     
-    pd = mpd4test(fullmodel, trnmodel, testdomain, trndomain, testi, gd, 1)
+    pd = mpd4test(fullmodel, trnmodel, testdomain, trndomain, testi, gd, 5)
     pdgrid = list2grid(pd,[101,101])
     y,x = testdomain
     print testdomain
@@ -111,3 +585,6 @@ def test1():
     
     plt.show()
     
+num = sys.argv[1]
+method = "test%s" %num
+getattr(sys.modules[__name__], method)()
