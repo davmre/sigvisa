d67b353f2846c42ce6f8bdb4664f8b53d6da0643
David Moore <dmoore@cs.berkeley.edu>
2012-09-26 17:35:17 -0700

    renamed for clarify

--- explore/inverted_detections_of_evid.py
+++ explore/inverted_detections_of_evid.py
@@ -5,7 +5,7 @@ import learn
 import sys
 import numpy as np
 
-orid = int(sys.argv[1])
+evid = int(sys.argv[1])
 
 cursor = database.db.connect().cursor()
 
@@ -13,7 +13,7 @@ sites = read_sites(cursor)
 phasenames, phasetimedef = read_phases(cursor)
 earthmodel = learn.load_earth("parameters", sites, phasenames, phasetimedef)
 
-cursor.execute("select og.time, og.lon, og.lat, og.depth, ss.id, ar.sta, ar.arid, ar.time, ar.iphase, ar.amp, ar.azimuth, ar.slow from leb_arrival ar, leb_assoc ac, leb_origin og, static_siteid ss  where ss.sta=ar.sta and ar.arid=ac.arid and ac.orid=og.orid and og.orid=%d and ss.statype='ar'" % (orid))
+cursor.execute("select og.time, og.lon, og.lat, og.depth, ss.id, ar.sta, ar.arid, ar.time, ar.iphase, ar.amp, ar.azimuth, ar.slow from leb_arrival ar, leb_assoc ac, leb_origin og, static_siteid ss  where ss.sta=ar.sta and ar.arid=ac.arid and ac.orid=og.orid and og.evid=%d and ss.statype='ar'" % (evid))
 
 res = cursor.fetchall()
 for r in res:
