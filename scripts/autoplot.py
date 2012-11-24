069f8975a274b7372cd2b383368267785fcd48e3
David Moore <dmoore@cs.berkeley.edu>
2012-11-23 20:19:40 -0800

    script for easy plotting

---
+++ scripts/autoplot.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib
+from optparse import OptionParser
+import os
+
+
+def main():
+    parser = OptionParser()
+    (options, args) = parser.parse_args()
+
+
+    if "DISPLAY" in os.environ:
+        matplotlib.use("TkAgg")
+        interactive = True
+        print "interactive mode..."
+    else:
+        matplotlib.use("Agg")
+        interactive = False
+        print "non-interactive mode..."
+    import matplotlib.pylab as plt
+
+    for wave_file_name in args:
+        x = np.loadtxt(wave_file_name)
+        plt.figure()
+        plt.plot(x)
+        plt.title(wave_file_name)
+
+        if not interactive:
+            save_fname = os.path.splitext(wave_file_name)[0] + '.png'
+            print "saving to ", save_fname
+            plt.savefig(save_fname)
+        else:
+            print "plotting", wave_file_name
+
+    print "done."
+    if interactive:
+        plt.show()
+
+if __name__ == "__main__":
+    main()
