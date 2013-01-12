48c2d93932d54ffb4b63e552c37ae04341697fe9
David Moore <davmre@gmail.com>
2013-01-12 04:48:31 +0000

    make fitting faster

---
+++ scripts/time_fitting.py
@@ -0,0 +1,25 @@
+import time
+
+import numpy as np
+import numpy.ma as ma
+
+
+from sigvisa import Sigvisa
+from signals.template_models.paired_exp import PairedExpTemplateModel
+from source.event import Event
+
+
+from learn.fit_shape_params import fit_event_segment, fit_template
+
+
+def main():
+    event = Event(evid=2781427) # Event(evid=5301405)
+    tm = PairedExpTemplateModel(run_name="", run_iter=0, model_type="dummy")
+
+    for optim_method in ('bfgs',):
+        t = time.time()
+        fit_event_segment(event=event, sta='FITZ', tm=tm, output_run_name="fit_profiling", output_iteration=1, plot=False, wiggles=None, iid=True, extract_wiggles=False, method=optim_method)
+        print "fit ev %d at %s in %f seconds using method %s." % (event.evid, "FITZ", time.time() - t, optim_method)
+
+if __name__ == "__main__":
+    main()
