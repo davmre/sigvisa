a7091cdee663f962059ed8e5dc7232ae94ee91bb
David Moore <davmre@gmail.com>
2013-02-11 20:48:27 -0800

    separate out sigvisa DB stuff

---
+++ database/sigvisa.sql
@@ -0,0 +1,201 @@
+
+create table sigvisa_coda_fitting_run (
+ runid 	     int,
+ run_name 	varchar(255),
+ iter 		int,
+ primary key(runid)
+);
+
+create table sigvisa_coda_fit (
+ /* fitid    int not null auto_increment, */ /* MYSQL version */
+ fitid	  int not null, /* Oracle version */
+ runid    int not null,
+ evid     int not null,
+ sta      varchar(10) not null,
+ chan     varchar(10) not null,
+ band     varchar(15) not null,
+ hz	  float(24),
+ optim_method  varchar(1024),
+ iid        int,
+ stime      double,
+ etime      double,
+ acost      float(24),
+ dist       float(24),
+ azi        float(24),
+ timestamp float(24),
+ elapsed   float(24),
+ human_approved varchar(1) default 0,
+ primary key(fitid),
+ foreign key(runid) REFERENCES sigvisa_coda_fitting_run(runid)
+);
+
+/* hack to implement auto_increment in Oracle */
+create sequence fitid_seq start with 1 increment by 1 nomaxvalue;
+create or replace trigger fitid_trigger
+before insert on sigvisa_coda_fit
+for each row
+begin
+select fitid_seq.nextval into :new.fitid from dual;
+end;
+/
+
+create table sigvisa_coda_fit_phase (
+/* fpid  int not null auto_increment, */ /* MYSQL version */
+ fpid  int not null, /* Oracle version */
+ fitid int not null,
+ phase	  varchar(20) not null,
+ template_model   varchar(20) default 'paired_exp',
+ param1	  double precision,
+ param2 double precision,
+ param3 double precision,
+ param4  double precision,
+ amp_transfer double precision,
+ wiggle_stime double precision,
+ wiggle_fname varchar(255),
+ primary key(fpid),
+ foreign key (fitid) REFERENCES sigvisa_coda_fit(fitid)
+);
+
+/* hack to implement auto_increment in Oracle */
+create sequence fpid_seq start with 1 increment by 1 nomaxvalue;
+create or replace trigger fpid_trigger
+before insert on sigvisa_coda_fit_phase
+for each row
+begin
+select fpid_seq.nextval into :new.fpid from dual;
+end;
+/
+
+create table sigvisa_wiggle (
+/* wiggleid  int not null auto_increment, */ /* MYSQL version */
+ wiggleid  int not null, /* Oracle version */
+ fpid int not null,
+ stime double precision not null,
+ etime double precision not null,
+ srate double precision not null,
+ timestamp double precision not null,
+ type varchar(31) not null,
+ log varchar(1) not null,
+ meta0 double precision,
+ meta1 double precision,
+ meta2 double precision,
+ meta3 double precision,
+ meta_str varchar(255),
+ params blob not null,
+ primary key(wiggleid),
+ foreign key (fpid) REFERENCES sigvisa_coda_fit_phase(fpid)
+);
+
+/* hack to implement auto_increment in Oracle */
+create sequence wiggleid_seq start with 1 increment by 1 nomaxvalue;
+create or replace trigger wiggleid_trigger
+before insert on sigvisa_wiggle
+for each row
+begin
+select wiggleid_seq.nextval into :new.wiggleid from dual;
+end;
+/
+
+create table sigvisa_template_param_model (
+ modelid int not null, /* Oracle version */
+ fitting_runid int not null,
+ template_shape varchar(15) not null,
+ param varchar(15) not null,
+ site varchar(10) not null,
+ chan varchar(10) not null,
+ band varchar(15) not null,
+ phase varchar(10) not null,
+ max_acost double precision not null,
+ min_amp double precision not null,
+ require_human_approved varchar(1) not null,
+ model_type varchar(31) not null,
+ model_fname varchar(255) not null,
+ training_set_fname varchar(255) not null,
+ n_evids int not null,
+ training_ll double precision not null,
+ timestamp double precision not null,
+ primary key (modelid),
+ foreign key (fitting_runid) REFERENCES sigvisa_coda_fitting_run(runid)
+);
+
+/* hack to implement auto_increment in Oracle */
+create sequence modelid_seq start with 1 increment by 1 nomaxvalue;
+create or replace trigger modelid_trigger
+before insert on sigvisa_template_param_model
+for each row
+begin
+select modelid_seq.nextval into :new.modelid from dual;
+end;
+/
+
+create table sigvisa_gridsearch_run (
+/* gsid  int not null auto_increment, */ /* MYSQL version */
+ gsid  int not null, /* Oracle version */
+ evid int not null,
+ timestamp double precision not null,
+ elapsed double precision not null,
+ lon_nw float(24) not null,
+ lat_nw float(24) not null,
+ lon_se float(24) not null,
+ lat_se float(24) not null,
+ pts_per_side int not null,
+ max_evtime_proposals int not null,
+ true_depth varchar(1) not null,
+ phases varchar(127) not null,
+ likelihood_method varchar(63) not null,
+ wiggle_model_type varchar(31) not null,
+ heatmap_fname varchar(255) not null,
+ primary key (gsid)
+);
+
+create table sigvisa_gsrun_wave (
+ gswid int not null, /* Oracle version */
+ gsid int not null,
+ sta varchar(10) not null,
+ chan varchar(10) not null,
+ band varchar(15) not null,
+ hz float(24) not null,
+ stime double precision not null,
+ etime double precision not null,
+ primary key (gswid),
+ foreign key (gsid) REFERENCES sigvisa_gridsearch_run(gsid)
+);
+
+create table sigvisa_gsrun_tmodel (
+ gsmid int not null, /* Oracle version */
+ gswid int not null,
+ modelid int not null,
+ primary key (gsmid),
+ foreign key (gswid) REFERENCES sigvisa_gsrun_wave(gswid),
+ foreign key (modelid) REFERENCES sigvisa_template_param_model(modelid)
+);
+
+/* hack to implement auto_increment in Oracle */
+create sequence gsid_seq start with 1 increment by 1 nomaxvalue;
+create or replace trigger gsid_trigger
+before insert on sigvisa_gridsearch_run
+for each row
+begin
+select gsid_seq.nextval into :new.gsid from dual;
+end;
+/
+
+/* hack to implement auto_increment in Oracle */
+create sequence gsw_id_seq start with 1 increment by 1 nomaxvalue;
+create or replace trigger gsw_id_trigger
+before insert on sigvisa_gsrun_wave
+for each row
+begin
+select gsw_id_seq.nextval into :new.gswid from dual;
+end;
+/
+
+/* hack to implement auto_increment in Oracle */
+create sequence gsm_id_seq start with 1 increment by 1 nomaxvalue;
+create or replace trigger gsm_id_trigger
+before insert on sigvisa_gsrun_tmodel
+for each row
+begin
+select gsm_id_seq.nextval into :new.gsmid from dual;
+end;
+/
