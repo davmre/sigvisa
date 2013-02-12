3b134b6eee4fa9cbdd9ed8963738e392218c22c2
David Moore <davmre@gmail.com>
2013-02-12 01:31:01 -0800

    rename web code and add navigation header (also, bugfixes)

--- web/waves/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-from waves.views import WaveImageView, WaveSelectView
-
-urlpatterns = patterns('',
-    url(r'^waves/$', WaveSelectView, name='wave_select'),
-    url(r'^waves/wave.png$',
-        WaveImageView,
-        name='wave_image'),
-)
