e3bd58ff75202a25a7e35ad6c273807270f99d95
David Moore <dmoore@cs.berkeley.edu>
2013-06-14 15:12:08 -0700

    merge node and dictnode (all nodes are now dictnodes). other node descendants have not been updated yet, so this breaks everything.

--- graph/nodes.py
+++ graph/nodes.py
@@ -6,118 +6,17 @@ from sigvisa.learn.train_param_common import load_modelid
 import sigvisa.infer.optimize.optim_utils as optim_utils
 from sigvisa.models import DummyModel
 
-
 class Node(object):
 
-
-    def __init__(self, model=None, label="", initial_value = None, fixed=False, children=None, parents = None):
+    def __init__(self, model=None, label="", initial_value = None, fixed=False, keys=None, children=None, parents = None):
 
         self.children = set(children) if children is not None else set()
         self.parents = parents if parents is not None else dict()
         self.model = model
-        self._value = initial_value
+        self._dict = initial_value
         self._fixed = fixed
         self.label = label
         self.mark = 0
-
-    def addChild(self, child):
-        self.children.add(child)
-        child.parents[self.label] = self
-
-    def addParent(self, parent):
-        parent.children.add(self)
-        self.parents[parent.label] = parent
-
-    def get_value(self):
-        return self._value
-
-    def set_value(self, value, override_fixed=False):
-        if not self._fixed or override_fixed:
-            self._value = value
-
-    def fix_value(self):
-        self._fixed = True
-
-    def unfix_value(self):
-        self._fixed = False
-
-    def _parent_values(self):
-        return dict([(k, p.get_value()) for (k, p) in self.parents.iteritems()])
-
-    def log_p(self, value=None, parent_values=None):
-        if parent_values is None:
-            parent_values = self._parent_values()
-        if value is None:
-            value = self.get_value()
-
-        return self.model.log_p(x = value, cond=parent_values)
-
-    def deriv_log_p(self, value=None, key=None, parent_values=None, parent_name=None, parent_key=None, **kwargs):
-        if parent_values is None:
-            parent_values = self._parent_values()
-        if value is None:
-            value = self.get_value()
-        return self.model.deriv_log_p(x = value, cond=parent_values, idx=key, cond_key=parent_name, cond_idx=parent_key, **kwargs)
-
-    def prior_sample(self, parent_values=None):
-        if self._fixed: return
-        if parent_values is None:
-            parent_values = self._parent_values()
-        new_value = self.model.sample(cond=parent_values)
-        self.set_value(new_value)
-
-    def prior_predict(self, parent_values=None):
-        if self._fixed: return
-        if parent_values is None:
-            parent_values = self._parent_values()
-        new_value = self.model.predict(cond=parent_values)
-        self.set_value(new_value)
-
-    def get_children(self):
-        return self.children
-
-    def set_mark(self, v=1):
-        self.mark = v
-
-    def clear_mark(self):
-        self.mark = 0
-
-    def get_mark(self):
-        return self.mark
-
-    # use custom getstate() and setstate() methods to avoid pickling
-    # param models when we pickle a graph object (since these models
-    # can be large, and GP models can't be pickled anyway).
-    def __getstate__(self):
-        try:
-            self.modelid
-            d = copy.copy(self.__dict__)
-            del d['model']
-            state = d
-        except AttributeError:
-            state = self.__dict__
-        return state
-
-    def __setstate__(self, state):
-        if "model" not in state:
-            if state['modelid'] is None:
-                state['model'] = DummyModel()
-            else:
-                state["model"] = load_modelid(modelid=state['modelid'])
-        self.__dict__.update(state)
-
-class DictNode(Node):
-
-    """
-
-    A Node whose value is guaranteed to be a vector (technically, a 1D
-    numpy array). Conceptually, models the joint distribution over some
-    set of variables.
-
-    """
-
-    def __init__(self, initial_value=None, fixed=None, keys=None, **kwargs):
-        super(DictNode, self).__init__(**kwargs)
 
         if keys:
             if initial_value:
@@ -137,12 +36,60 @@ class DictNode(Node):
             raise ValueError("passed invalid fixed-values setting %s" % fixed)
         self._fixed = not any(self._mutable.itervalues())
 
-        self._value = initial_value
+        self.single_key = None
+        if len(keys) == 1:
+            self.single_key = keys[0]
+
+    def addChild(self, child):
+        self.children.add(child)
+        child.parents[self.label] = self
+
+    def addParent(self, parent):
+        parent.children.add(self)
+        self.parents[parent.label] = parent
+
+    def deterministic(self):
+        # deterministic nodes are treated specially (see
+        # DeterministicNode below).
+        return False
 
     def keys(self):
-        return sorted(self._mutable.keys())
+        # return the list of keys provided by this node
+        return sorted(self._dict.keys())
+
+    def get_value(self, key):
+        return self._dict[key]
+
+    def set_value(self, value, key):
+        if self._mutable[key]:
+            self._dict[key] = value
+
+    def get_dict(self):
+        return self._dict
+
+    def set_dict(self, value, override_fixed=False):
+        assert(set(value.iterkeys()) == set(self._mutable.iterkeys()))
+        if override_fixed:
+            self._dict = value
+        else:
+            self._dict = {k : value[k] if self._mutable[k] else self._dict[k] for k in value.iterkeys() }
+
+    def set_values_from_model(self, value):
+        # here "value" is assumed to be whatever is returned when
+        # sampling from the model at this node. it can be a single
+        # value or a dict/vector of values. in the latter case, we'll
+        # generally need to override this method to do the right thing
+        # with respect to whatever the model actually returns.
+
+        if self.single_key:
+            self.set_value(value, self.single_key)
+        else:
+            self.set_all_values(value)
 
     def fix_value(self, key=None):
+        # hold constant (i.e., do not allow to be set or resampled) the value
+        # of a particular key at this node, or of all keys (if no key is specified)
+
         if key is None:
             self._mutable = {k : False for k in self._mutable.iterkeys() }
         else:
@@ -156,27 +103,98 @@ class DictNode(Node):
             self._mutable[key] = True
         self._fixed = False
 
+    def _parent_values(self):
+        # return a dict of all keys provided by parent nodes, and their values
+        return dict([(k, v) for p in self.parents.items() for (k,v) in p.items()])
+
+    def log_p(self, parent_values=None):
+        #  log probability of the values at this node, conditioned on all parent values
+
+        if parent_values is None:
+            parent_values = self._parent_values()
+        v = self.get_dict()
+        v = v[self.single_key] if self.single_key else v
+        return self.model.log_p(x = v, cond=parent_values)
+
+    def deriv_log_p(self, key=None, parent_values=None, parent_key=None, parent_idx=None,  **kwargs):
+        # derivative of the log probability at this node, with respect
+        # to a key at this node, or with respect to a key provided by
+        # a parent.
+        if parent_values is None:
+            parent_values = self._parent_values()
+
+        v = self.get_dict()
+        v = v[self.single_key] if self.single_key else v
+        return self.model.deriv_log_p(x = v, cond=parent_values, idx=key, cond_key=parent_key, cond_idx=parent_idx, **kwargs)
+
+    def parent_sample(self, parent_values=None):
+        # sample a new value at this node conditioned on its parents
+        if self._fixed: return
+        if parent_values is None:
+            parent_values = self._parent_values()
+        self._set_value_from_model(self.model.sample(cond=parent_values))
+
+    def parent_predict(self, parent_values=None):
+        # predict a new value at this node conditioned on its parents.
+        # the meaning of "predict" varies with the model, but is
+        # usually the mean or mode of the conditional distribution.
+        if self._fixed: return
+        if parent_values is None:
+            parent_values = self._parent_values()
+        self._set_value_from_model(self.model.predict(cond=parent_values))
+
+    def get_children(self):
+        return self.children
+
+    def set_mark(self, v=1):
+        self.mark = v
+
+    def clear_mark(self):
+        self.mark = 0
+
+    def get_mark(self):
+        return self.mark
+
+    # use custom getstate() and setstate() methods to avoid pickling
+    # param models when we pickle a graph object (since these models
+    # can be large, and GP models can't be directly pickled anyway).
+    def __getstate__(self):
+        try:
+            self.modelid
+            d = copy.copy(self.__dict__)
+            del d['model']
+            state = d
+        except AttributeError:
+            state = self.__dict__
+        return state
+
+    def __setstate__(self, state):
+        if "model" not in state:
+            if state['modelid'] is None:
+                state['model'] = DummyModel()
+            else:
+                state["model"] = load_modelid(modelid=state['modelid'])
+        self.__dict__.update(state)
+
     def mutable_dimension(self):
+        # return the number of values at this node that are not fixed
+        # (and can thus be optimized over, etc).
         return sum(self._mutable.itervalues())
 
     def mutable_keys(self):
+        # return the set of keys at this node whose values are not
+        # fixed (and can thus be optimized over, etc).
         return [k for k in self.keys() if self._mutable[k]]
 
     def get_mutable_values(self):
-        """
-        Get the values of all of the sub-variables that are not fixed. (used for optimization, etc.)
-        """
-        return [self._value[k] for k in self.keys() if self._mutable[k]]
+        return [self._dict[k] for k in self.keys() if self._mutable[k]]
 
     def set_mutable_values(self, values):
-        """
-        Set the values of all of the sub-variables that are not fixed. (used for optimization, etc.)
-        """
         assert(len(values) == self.mutable_dimension())
         i = 0
         for k in self.keys():
             if self._mutable[k]:
-                self._value[k] = values[i]
+                self._dict[k] = values[i]
                 i += 1
 
     def low_bounds(self):
@@ -185,123 +203,29 @@ class DictNode(Node):
     def high_bounds(self):
         return [np.float('inf'),] * self.mutable_dimension()
 
-    def set_value(self, value, override_fixed=False):
-        assert(set(value.iterkeys()) == set(self._mutable.iterkeys()))
-        if override_fixed:
-            self._value = value
-        else:
-            self._value = {k : value[k] if self._mutable[k] else self._value[k] for k in value.iterkeys() }
 
-    def set_key(self, value, key):
-        if self._mutable[key]:
-            self._value[key] = value
+class DeterministicNode(Node):
 
-
-class ClusterNode(DictNode):
-
-    """
-
-    A collection of Nodes, each having the same parents and children,
-    which can therefore be collapsed into a single node conceptually, even
-    though they are still treated as independent for computational
-    purposes.
-
-    """
-
-    def __init__(self, nodes, label="", parents = None, children=None):
-
-        # setup below here assumes that the set of nodes is finalized,
-        # so subclasses should only call this constructor after
-        # constructing all sub-nodes.
-        assert( isinstance(nodes, dict) )
-        self._nodes = nodes
-
-        # invariant: self._mutable[i] == self._nodes[i]._fixed for all nodes i
-        super(ClusterNode, self).__init__(model=None, label=label,
-                                          parents=parents, children=children,
-                                          fixed = { k : nodes[k]._fixed for k in nodes.keys() })
-
-        for node in self._nodes.itervalues():
-            node.parents = self.parents
-            node.children = self.children
-
-        self._value = None
-
-    def fix_value(self, key=None):
-        if key is None:
-            for node in self._nodes.itervalues():
-                node.fix_value()
-            self._mutable = {k : False for k in self._mutable.iterkeys() }
-        else:
-            self._nodes[key].fix_value()
-            self._mutable[key] = False
-        self._fixed = not any(self._mutable.itervalues())
-
-    def unfix_value(self, key=None):
-        if key is None:
-            for node in self._nodes.itervalues():
-                node.unfix_value()
-            self._mutable = {k : True for k in self._mutable.iterkeys() }
-        else:
-            self._nodes[key].unfix_value()
-            self._mutable[key] = True
-        self._fixed = False
-
-    def get_mutable_values(self):
-        values = []
-        for (k,n) in sorted(self._nodes.iteritems()):
-            if self._mutable[k]:
-                values.append(n.get_value())
-        return values
-
-    def set_mutable_values(self, values):
-        assert(len(values) == self.mutable_dimension())
-        i = 0
-        for k in self.keys():
-            if self._mutable[k]:
-                self._nodes[k].set_value(values[i])
-                i += 1
-
-    def get_value(self):
-        return { k : self._nodes[k].get_value() for k in self._nodes.iterkeys() }
-
-    def set_value(self, value, override_fixed=False):
-        assert(set(value.iterkeys()) == set(self._mutable.iterkeys()))
-        for (k, v) in value.iteritems():
-            self._nodes[k].set_value(v, override_fixed=override_fixed)
-
-    def set_key(self, value, key):
-        if self._mutable[key]:
-            self._nodes[key].set_value(value = value)
-
-    def log_p(self, value = None):
-        if value is None:
-            value = self.get_value()
-
-        lp = sum( [self._nodes[key].log_p(value = value[key]) for key in self.keys() ] )
-        return lp
-
-    def deriv_log_p(self, value = None, key=None, **kwargs):
-        if value is None:
-            value = self.get_value()
-
-        # ignore initial log_prob optimization since it doesn't
-        # (currently) decompose across clusternodes
-        if 'lp0' in kwargs:
-            del kwargs['lp0']
-
-        return self._nodes[key].deriv_log_p(value = value[key], **kwargs)
+    def deterministic(self):
+        return True
 
     def prior_sample(self, parent_values=None):
-        if parent_values is None:
-            parent_values = self._parent_values()
-
-        for node in self._nodes.itervalues():
-            node.prior_sample(parent_values = parent_values)
+        self.compute_value(parent_values=parent_values)
 
     def prior_predict(self, parent_values=None):
-        if parent_values is None:
-            parent_values = self._parent_values()
+        self.compute_value(parent_values=parent_values)
 
-        for node in self._nodes.itervalues():
-            node.prior_predict(parent_values = parent_values)
+    def log_p(self, value=None, parent_values=None):
+        return AttributeError("cannot compute log_p for a deterministic node!")
+
+    def deriv_log_p(**kwargs):
+        return AttributeError("cannot compute deriv_log_p for a deterministic node!")
+
+    def set_value(self, value, override_fixed=False):
+        return AttributeError("cannot set value for a deterministic node!")
+
+    def fix_value(self):
+        return AttributeError("cannot fix/unfix values for a deterministic node!")
+
+    def unfix_value(self):
+        return AttributeError("cannot fix/unfix values for a deterministic node!")
