772e54a044976e019cc3b9879b5d2c6984d29f73
David Moore <dmoore@cs.berkeley.edu>
2013-07-16 18:23:47 -0700

    kill new_exp

--- models/templates/load_by_name.py
+++ models/templates/load_by_name.py
@@ -1,10 +1,8 @@
 from sigvisa.models.templates.paired_exp import PairedExpTemplateGenerator
-from sigvisa.models.templates.new_exp import NewExpTemplateGenerator
+#from sigvisa.models.templates.new_exp import NewExpTemplateGenerator
 
 def load_template_generator(template_shape, **kwargs):
     if template_shape == "paired_exp":
         return PairedExpTemplateGenerator(**kwargs)
-    elif template_shape == "new_exp":
-        return NewExpTemplateGenerator(**kwargs)
     else:
         raise KeyError("unknown template shape %s" % template_shape)
