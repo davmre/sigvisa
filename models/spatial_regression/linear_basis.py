bfa595ca4a0c63dcf28d5acc4151c5acf5e987eb
David Moore <dmoore@cs.berkeley.edu>
2013-11-27 00:17:58 -0800

    move linear features code into sparsegp

--- models/spatial_regression/linear_basis.py
+++ models/spatial_regression/linear_basis.py
@@ -11,7 +11,7 @@ import marshal
 import types
 
 from sigvisa.models.spatial_regression.baseline_models import ParamModel
-from sigvisa.models.spatial_regression.features import featurizer_from_string, recover_featurizer
+from sigvisa.sparsegp.features import featurizer_from_string, recover_featurizer
 
 class LinearBasisModel(ParamModel):
 
