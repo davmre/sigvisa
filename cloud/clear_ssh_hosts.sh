21f636686ac20a6c9fea23263cdfda3bcb75145c
Dave Moore <dmoore@cs.berkeley.edu>
2015-08-12 18:10:36 -0700

    cloud fixes including swapfile for memory exhaustion

---
+++ cloud/clear_ssh_hosts.sh
@@ -0,0 +1,4 @@
+ssh-keygen -f "/home/dmoore/.ssh/known_hosts" -R sigvisa1.cloudapp.net
+ssh-keygen -f "/home/dmoore/.ssh/known_hosts" -R sigvisa2.cloudapp.net
+ssh-keygen -f "/home/dmoore/.ssh/known_hosts" -R sigvisa3.cloudapp.net
+ssh-keygen -f "/home/dmoore/.ssh/known_hosts" -R sigvisa4.cloudapp.net
