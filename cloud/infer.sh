d0e7be86c258d95c71fe1ba31b0ab7ed0ebdd51c
Dave Moore <dmoore@cs.berkeley.edu>
2016-02-04 18:31:47 -0800

    cloud image updates

--- cloud/infer.sh
+++ cloud/infer.sh
@@ -4,4 +4,4 @@ source /home/sigvisa/.bash_profile
 source /home/sigvisa/.virtualenvs/sigvisa/bin/activate
 
 cd $SIGVISA_HOME
-python experiments/ctf_real.py $@ >> /home/sigvisa/sigvisa_log.txt 2>&1
+python $@ >> /home/sigvisa/sigvisa_log.txt 2>&1
